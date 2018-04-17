b7534bc7b35b369ba06af7b864e766f9b75cf020
michael.jolin <michael.jolin@cauca.ca>
2018-04-17 10:47:02 -0400

    refactor: Use BaseCrud (Service and Controller) for state

--- Survi.Prevention.ServiceLayer/Services/StateService.cs
+++ Survi.Prevention.ServiceLayer/Services/StateService.cs
@@ -7,13 +7,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
-    public class StateService : BaseService
+    public class StateService : BaseCrudService<State>
     {
         public StateService(ManagementContext context) : base(context)
         {
         }
 
-        public State Get(Guid id)
+        public override State Get(Guid id)
         {
             var result = Context.States
                         .Include(s => s.Localizations)
@@ -22,41 +22,13 @@ namespace Survi.Prevention.ServiceLayer.Services
             return result;
         }
 
-        public List<State> GetList()
+        public override List<State> GetList()
         {
             var result = Context.States
                         .Include(s => s.Localizations)
                         .ToList();
 
             return result;
-        }
-
-        public Boolean AddOrUpdate(State state)
-        {
-            var isExistRecord = Context.States.Any(s => s.Id == state.Id);
-
-            if (isExistRecord)
-            {
-                Context.States.Update(state);
-            }
-            else
-            {
-                Context.States.Add(state);
-            }
-            
-            Context.SaveChanges();
-
-            return true;
-        }
-
-        public Boolean Remove(Guid id)
-        {
-            var state = Context.States.Find(id);
-
-            state.IsActive = false;
-            Context.SaveChanges();
-
-            return true;
         }
     }
 }
