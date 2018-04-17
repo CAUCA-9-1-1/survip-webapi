a356ccb9dd644133eacf8f942919ec77e50cef2e
michael.jolin <michael.jolin@cauca.ca>
2018-04-17 10:56:08 -0400

    feat: add BaseCrudService and BaseCrudController and refactor CountryService and CountryController to use those abstract classes.

--- Survi.Prevention.ServiceLayer/Services/CountryService.cs
+++ Survi.Prevention.ServiceLayer/Services/CountryService.cs
@@ -7,13 +7,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
-	public class CountryService : BaseService
+	public class CountryService : BaseCrudService<Country>
 	{
 		public CountryService(ManagementContext context) : base(context)
 		{
 		}
 
-		public Country Get(Guid id)
+		public override Country Get(Guid id)
 		{
 			var result = Context.Countries
 				.Include(c => c.Localizations)
@@ -22,41 +22,13 @@ namespace Survi.Prevention.ServiceLayer.Services
 			return result;
 		}
 
-		public List<Country> GetList()
+		public override List<Country> GetList()
 		{
 			var result = Context.Countries
 				.Include(c => c.Localizations)
 				.ToList();
 
 			return result;
-		}
-
-		public Boolean AddOrUpdate(Country country)
-		{
-			var isExistRecord = Context.Countries.Any(c => c.Id == country.Id);
-
-			if (isExistRecord)
-			{
-				Context.Countries.Update(country);
-			}
-			else
-			{
-				Context.Countries.Add(country);
-			}
-
-			Context.SaveChanges();
-
-			return true;
-		}
-
-		public Boolean Remove(Guid id)
-		{
-			var country = Context.Countries.Find(id);
-
-			country.IsActive = false;
-			Context.SaveChanges();
-
-			return true;
-		}
+		}		
 	}
 }
---
+++ Survi.Prevention.WebApi/Controllers/BaseCrudController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.Base;
+using Survi.Prevention.Models.FireSafetyDepartments;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+	[Produces("application/json")]
+	public abstract class BaseCrudController<TService, TModel> : BaseSecuredController
+		where TModel : BaseModel
+		where TService : BaseCrudService<TModel>
+	{
+		protected readonly TService Service;
+
+		protected BaseCrudController(TService service)
+		{
+			this.Service = service;
+		}
+
+		[HttpGet]
+		[Route("{id:Guid}")]
+		[ProducesResponseType(401)]
+		[ProducesResponseType(typeof(Country), 200)]
+		public ActionResult Get(Guid id)
+		{
+			var entity = Service.Get(id);
+			return Ok(entity);
+		}
+
+		[HttpGet]
+		[ProducesResponseType(401)]
+		[ProducesResponseType(typeof(List<Country>), 200)]
+		public ActionResult Get()
+		{
+			var result = Service.GetList();
+
+			if (result.Count > 0)
+				return Ok(result);
+
+			return BadRequest();  // est-ce vraiment un BadRequest s'il n'y a rien à retourner?
+		}
+
+		[HttpPost]
+		[ProducesResponseType(401)]
+		[ProducesResponseType(200)]
+		public ActionResult Post([FromBody] TModel entity)
+		{
+			if (Service.AddOrUpdate(entity))
+				return Ok();
+
+			return BadRequest();
+		}
+
+		[HttpDelete]
+		[Route("{id:Guid}")]
+		[ProducesResponseType(401)]
+		[ProducesResponseType(200)]
+		public ActionResult Delete(Guid id)
+		{
+			if (Service.Remove(id))
+				return Ok();
+
+			return BadRequest();
+		}
+	}
+}
