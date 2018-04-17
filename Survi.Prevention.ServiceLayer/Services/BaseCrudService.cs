c56300bcdcacae97d0edefc96663708ad3f46c24
michael.jolin <michael.jolin@cauca.ca>
2018-04-17 10:56:08 -0400

    feat: add BaseCrudService and BaseCrudController and refactor CountryService and CountryController to use those abstract classes.

---
+++ Survi.Prevention.ServiceLayer/Services/BaseCrudService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Survi.Prevention.DataLayer;
+using Survi.Prevention.Models.Base;
+
+namespace Survi.Prevention.ServiceLayer.Services
+{
+	public abstract class BaseCrudService<T> : BaseService
+		where T : BaseModel
+	{
+		protected BaseCrudService(ManagementContext context) 
+			: base(context)
+		{
+		}
+
+		public bool AddOrUpdate(T entity)
+		{
+			var isExistRecord = Context.Set<T>().Any(c => c.Id == entity.Id);
+
+			if (isExistRecord)
+				Context.Set<T>().Update(entity);
+			else
+				Context.Set<T>().Add(entity);
+
+			Context.SaveChanges();
+			return true;
+		}
+
+		public bool Remove(Guid id)
+		{
+			var entity = Context.Set<T>().Find(id);
+			entity.IsActive = false;
+			Context.SaveChanges();
+
+			return true;
+		}
+
+		public abstract T Get(Guid id);
+		public abstract List<T> GetList();
+	}
+}
--- Survi.Prevention.WebApi/Controllers/CountryController.cs
+++ Survi.Prevention.WebApi/Controllers/CountryController.cs
@@ -6,63 +6,11 @@ using Survi.Prevention.ServiceLayer.Services;
 
 namespace Survi.Prevention.WebApi.Controllers
 {
-	[Produces("application/json")]
 	[Route("api/Country")]
-	public class CountryController : BaseSecuredController
-	{
-		private readonly CountryService service;
-
-		public CountryController([FromServices] CountryService service)
+	public class CountryController : BaseCrudController<CountryService, Country>
+	{		
+		public CountryController(CountryService service) : base(service)
 		{
-			this.service = service;
-		}
-
-        [HttpGet]
-        [Route("{id:Guid}")]
-        [ProducesResponseType(typeof(Country), 200)]
-        public ActionResult Get(Guid id)
-        {
-            var country = service.Get(id);
-            return Ok(country);
-        }
-
-        [HttpGet]
-        [ProducesResponseType(typeof(List<Country>), 200)]
-        public ActionResult Get()
-        {
-            var result = service.GetList();
-
-			if (result.Count > 0)
-			{
-				return Ok(result);
-			}
-
-			return BadRequest();
-		}
-
-        [HttpPost]
-        [ProducesResponseType(200)]
-        public ActionResult Post([FromBody] Country country)
-        {
-            if (service.AddOrUpdate(country))
-            {
-                return Ok();
-            }
-
-			return BadRequest();
-		}
-
-        [HttpDelete]
-        [Route("{id:Guid}")]
-        [ProducesResponseType(200)]
-        public ActionResult Delete(Guid id)
-        {
-            if (service.Remove(id))
-            {
-                return Ok();
-            }
-
-			return BadRequest();
 		}
 	}
 }
