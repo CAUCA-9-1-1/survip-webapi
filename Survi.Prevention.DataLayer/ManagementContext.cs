4720a456659ad371c87b36191a19ce2171132274
Philippe Robert <philippe.robert@cauca.ca>
2018-04-25 11:09:02 -0400

    feat #22 Correctif de déclarations multiple suite au merge de la branche management-fire-hydrant

--- Survi.Prevention.DataLayer/ManagementContext.cs
+++ Survi.Prevention.DataLayer/ManagementContext.cs
@@ -38,8 +38,6 @@ namespace Survi.Prevention.DataLayer
         public DbSet<FireSafetyDepartment> FireSafetyDepartments { get; set; }
         public DbSet<Firestation> Firestations { get; set; }
         public DbSet<UnitOfMeasure> UnitOfMeasures { get; set; }
-        public DbSet<FireSafetyDepartment> FireSafetyDepartments { get; set; }
-        public DbSet<Firestation> Firestations { get; set; }
 
 		public DbSet<ConstructionType> ConstructionTypes { get; set; }
 		public DbSet<RoofType> RoofTypes { get; set; }
@@ -53,12 +51,8 @@ namespace Survi.Prevention.DataLayer
 		public DbSet<FireHydrantConnectionType> FireHydrantConnectionTypes { get; set; }
 		public DbSet<OperatorType> OperatorTypes { get; set; }
 		public DbSet<ConstructionFireResistanceType> ConstructionFireResistanceTypes { get; set; }
-		public DbSet<UnitOfMeasure> UnitOfMeasures { get; set; }
 
         public DbSet<FireHydrant> FireHydrants { get; set; }
-        public DbSet<FireHydrantType> FireHydrantTypes { get; set; }
-        public DbSet<OperatorType> OperatorTypes { get; set; }
-        public DbSet<FireHydrantConnectionType> FireHydrantConnectionTypes { get; set; }
 
 		public ManagementContext(DbContextOptions<ManagementContext> options) : base(options)
 		{
