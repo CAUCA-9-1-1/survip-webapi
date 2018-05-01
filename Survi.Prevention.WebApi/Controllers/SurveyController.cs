3e792eb8b676779cb357b9d4e23e0e42b2ac0e57
Philippe Robert <philippe.robert@cauca.ca>
2018-05-01 08:52:25 -0400

    feat(#75): Create survey controller and service

---
+++ Survi.Prevention.WebApi/Controllers/SurveyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.SurveyManagement;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+	[Route("api/Survey")]
+	public class SurveyController : BaseCrudController<SurveyService, Survey>
+	{
+		public SurveyController(SurveyService service) : base(service)
+		{
+		}
+	}
+}
