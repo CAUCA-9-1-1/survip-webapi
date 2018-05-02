59fde7fc2d96dcd2be41f101074405381c1ebdf4
frederic.catellier <syd_freak@hotmail.com>
2018-05-02 16:04:07 -0400

    feat(#16) Add controller to get all construction  types from the api.

---
+++ Survi.Prevention.WebApi/Controllers/ConstructionController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+	[Route("api/construction")]
+	public class ConstructionController : BaseSecuredController
+	{
+		private readonly ConstructionService service;
+
+		protected ConstructionController(ConstructionService service)
+		{
+			this.service = service;
+		}
+
+		public ActionResult GetBuildingSidingTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetBuildingSidingTypes(languageCode));
+		}
+
+		public ActionResult GetBuildingTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetBuildingTypes(languageCode));
+		}
+
+		public ActionResult GetConstructionFireResistanceTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetConstructionFireResistanceTypes(languageCode));
+		}
+
+		public ActionResult GetConstructionTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetConstructionTypes(languageCode));
+		}
+
+		public ActionResult GetRoofMaterialTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetRoofMaterialTypes(languageCode));
+		}
+
+		public ActionResult GetRoofTypes([FromHeader] string languageCode)
+		{
+			return Ok(service.GetRoofTypes(languageCode));
+		}
+	}
+}
