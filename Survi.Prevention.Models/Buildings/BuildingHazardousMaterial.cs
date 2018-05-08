299ebcfb59e0278b4306635e022486b1bab3d8d6
frederic.catellier <syd_freak@hotmail.com>
2018-05-08 09:47:20 -0400

    Feat(#20) Add missing fields.

--- Survi.Prevention.Models/Buildings/BuildingHazardousMaterial.cs
+++ Survi.Prevention.Models/Buildings/BuildingHazardousMaterial.cs
@@ -3,16 +3,28 @@ using Survi.Prevention.Models.Base;
 
 namespace Survi.Prevention.Models.Buildings
 {
+	public enum StorageTankType
+	{
+		Unknown,
+		Underground,
+		Aboveground
+	} 
+
+
 	public class BuildingHazardousMaterial : BaseModel
 	{
 		public int Quantity { get; set; }
 		public string Container { get; set; }
-		public string CapacityContainer { get; set; }
+		public decimal CapacityContainer { get; set; }
 		public string Place { get; set; }
+		public string Wall { get; set; }
+		public string Sector { get; set; }
 		public string Floor { get; set; }
 		public string GasInlet { get; set; }
 		public string SecurityPerimeter { get; set; }
 		public string OtherInformation { get; set; }
+		public StorageTankType TankType { get; set; }
+		public string SupplyLine { get; set; }
 
 		public Guid IdBuilding { get; set; }
 		public Guid IdHazardousMaterial { get; set; }
