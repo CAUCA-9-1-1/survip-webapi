cbd77a02a7acebec8d2904269605d40b1e89f528
frederic.catellier <syd_freak@hotmail.com>
2018-05-11 11:45:38 -0400

    Feat(#23) Add support for anomalies management.

---
+++ Survi.Prevention.Models/DataTransfertObjects/BuildingAnomalyForList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Survi.Prevention.Models.DataTransfertObjects
+{
+    public class BuildingAnomalyForList
+    {
+		public Guid Id { get; set; }
+		public string Theme { get; set; }
+		public string Notes { get; set; }
+    }
+}
---
+++ Survi.Prevention.Models/DataTransfertObjects/InspectionBuildingAnomalyThemeForList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Survi.Prevention.Models.DataTransfertObjects
+{
+    public class InspectionBuildingAnomalyThemeForList
+    {
+		public string Theme { get; set; }
+		public List<BuildingAnomalyForList> Anomalies { get; set; }
+    }
+}
