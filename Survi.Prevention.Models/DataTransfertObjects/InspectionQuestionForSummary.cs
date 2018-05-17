2295640ba5ef5ce0bdc2217634a0157de005e784
Philippe Robert <philippe.robert@cauca.ca>
2018-05-17 14:03:13 -0400

    feat(#45) : Add group by in answer summary

--- Survi.Prevention.Models/DataTransfertObjects/InspectionQuestionForSummary.cs
+++ Survi.Prevention.Models/DataTransfertObjects/InspectionQuestionForSummary.cs
@@ -6,7 +6,10 @@ namespace Survi.Prevention.Models.DataTransfertObjects
 	public class InspectionQuestionForSummary
 	{
 		public Guid Id { get; set; }
-		public string QuestionDescription { get; set; }
+		public string QuestionTitle { get;  set; } = "";
+		public string QuestionDescription { get; set; } = "";
 		public string Answer { get; set; } = "";
+		public int QuestionType { get; set; }
+		public int Sequence { get; set; }
 	}
 }
