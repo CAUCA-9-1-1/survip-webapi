25b60bba4ad478ac5809e141704fcfd2ad9d56fa
Julien Landry <julien.r.lan@googlemail.com>
2018-07-13 09:09:39 -0400

    fix(#177): Change controller for conformity with CRUD

---
+++ Survi.Prevention.WebApi/Controllers/ReportConfigurationTemplateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+    [Route("api/ReportConfigurationTemplate")]
+    public class ReportConfigurationTemplateController : BaseCrudController<ReportConfigurationTemplateService, ReportConfigurationTemplate>
+    {
+        
+        private ReportConfigurationTemplateService Service;
+        
+        public ReportConfigurationTemplateController(ReportConfigurationTemplateService service) : base(service)
+        {
+            Service = service;
+        }
+    }
+}
