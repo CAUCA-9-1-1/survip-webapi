85770b4c2a127768579a729aeb302d3ad83e64bc
Julien Landry <julien.r.lan@googlemail.com>
2018-07-26 20:43:04 -0400

    feat(#149): PDF generation and beginning of placeholders

---
+++ Survi.Prevention.ServiceLayer/Report.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Survi.Prevention.Models
+{
+    public class Report
+    {
+        public string Address {get; set; }
+        public string ZipCode {get; set; }
+        
+        public string ReplacePlaceholders(string template)
+        {
+            var res = Regex.Replace(template, "{{(.*?)}}", m => GetValue(ref m));
+            return res;
+        }
+
+        private string GetValue(ref Match m)
+        {
+            try
+            {
+                return 
+                    GetType()
+                    .GetProperty(m.Groups[1].Value)
+                    .GetValue(this)
+                    .ToString();
+            }
+            catch (NullReferenceException e)
+            {
+                return m.Groups[1].Value;
+            }
+        }
+    }
+}
--- Survi.Prevention.ServiceLayer/Services/ReportGenerationService.cs
+++ Survi.Prevention.ServiceLayer/Services/ReportGenerationService.cs
@@ -2,17 +2,18 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
-using System.Net.Mime;
-using System.Reflection;
 using Survi.Prevention.DataLayer;
 using Survi.Prevention.Models;
 using System.Text;
+using System.Text.RegularExpressions;
 using WkWrap;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
     public class ReportGenerationService : BaseCrudService<ReportConfigurationTemplate>
-    {        
+    {
+        private const double KMarginInMM = 19.05;
+
         public ReportGenerationService(ManagementContext context) : base(context)
         {
         }
@@ -32,28 +33,7 @@ namespace Survi.Prevention.ServiceLayer.Services
 
             return result;
         }
-
-        public MemoryStream Generate(Guid id)
-        {
-            var output = new MemoryStream();
-            var input = new MemoryStream();
-            var uniEncoding = new UnicodeEncoding();
-            var sw = new StreamWriter(input, uniEncoding);
-
-            var template = GetTemplate();
-            sw.Write(template.Data);
-            sw.Flush();    // Prevents getting an empty stream
-            input.Seek(0, SeekOrigin.Begin);
-
-            var wkhtmltopdf = new FileInfo(@"/usr/bin/wkhtmltopdf");
-            var converter = new HtmlToPdfConverter(wkhtmltopdf);
-            converter.ConvertToPdf(input, output);
-            output.Seek(0, SeekOrigin.Begin);
-            sw.Dispose();
-            
-            return output;
-        }
-
+        
         private ReportConfigurationTemplate GetTemplate()
         {
             var result = Context.ReportConfigurationTemplate
@@ -61,5 +41,45 @@ namespace Survi.Prevention.ServiceLayer.Services
             var template = result.ElementAt(0);
             return template;
         }
+
+        public MemoryStream Generate(Guid id)
+        {
+            var inputStream = new MemoryStream();
+            var outputStream = new MemoryStream();
+            var streamWriter = new StreamWriter(inputStream, new UnicodeEncoding());
+
+            var template = GetTemplate();
+            var inspectionTextReport = report.ReplacePlaceholders(template.Data);
+            streamWriter.Write(inspectionTextReport);
+            streamWriter.Flush();    // Prevents getting an empty stream
+            inputStream.Seek(0, SeekOrigin.Begin);
+
+            var settings = new ConversionSettings
+            {
+                PageSize = PageSize.Letter,
+                Margins = new PageMargins
+                {
+                    Bottom = KMarginInMM,
+                    Top = KMarginInMM,
+                    Left = KMarginInMM,
+                    Right = KMarginInMM
+                }
+            };
+            var wkhtmltopdf = new FileInfo(@"/usr/bin/wkhtmltopdf");
+            var converter = new HtmlToPdfConverter(wkhtmltopdf);
+            converter.ConvertToPdf(inputStream, outputStream, settings);
+            outputStream.Seek(0, SeekOrigin.Begin);
+            streamWriter.Dispose();
+            
+            return outputStream;
+        }
+        
+
+        
+        private Report report = new Report
+        {
+            Address = "2712 Rue Linton",
+            ZipCode = "H2L 9N2"
+        };
     }
 }
