8173615ef8640355302751bccdbe1e2e62f4251d
Unknown <syd_freak@hotmail.com>
2018-08-17 11:58:59 -0400

    Feat(#25) Add inspection's configuration fields to the inspection detail.

---
+++ Survi.Prevention.Models/DataTransfertObjects/InspectionConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Survi.Prevention.Models.DataTransfertObjects
+{
+    public class InspectionConfiguration
+    {
+		public Boolean HasGeneralInformation { get; set; } = true;
+		public Boolean HasImplantationPlan { get; set; } = true;
+	    public Boolean HasCourse { get; set; }
+	    public Boolean HasWaterSupply { get; set; }
+		public Boolean HasBuildingDetails { get; set; } = true;
+	    public Boolean HasBuildingContacts { get; set; }
+	    public Boolean HasBuildingPnaps { get; set; }
+	    public Boolean HasBuildingFireProtection { get; set; }
+	    public Boolean HasBuildingHazardousMaterials { get; set; }
+	    public Boolean HasBuildingParticularRisks { get; set; }
+	    public Boolean HasBuildingAnomalies { get; set; }
+	}
+}
