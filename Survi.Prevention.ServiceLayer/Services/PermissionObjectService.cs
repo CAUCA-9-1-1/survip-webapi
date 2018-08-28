88e497605d21519c5b5e6a0563190b89ba9709d1
michael.jolin <michael.jolin@cauca.ca>
2018-08-28 13:06:06 -0400

    feat(#73): Allow to save and remove permission object

--- Survi.Prevention.ServiceLayer/Services/PermissionObjectService.cs
+++ Survi.Prevention.ServiceLayer/Services/PermissionObjectService.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Data.Common;
 using Survi.Prevention.DataLayer;
 using Survi.Prevention.Models.SecurityManagement;
 
@@ -17,7 +17,49 @@ namespace Survi.Prevention.ServiceLayer.Services
 			var permissionObjects = Context.PermissionObjects
 				.ToList();
 
+			permissionObjects.ForEach(obj =>
+			{
+				if (obj.ObjectTable != "webuser")
+				{
+					return;
+				}
+
+				obj.GroupName = Context.WebuserAttributes
+					.First(a => a.IdWebuser.ToString() == obj.GenericId && a.AttributeName == "first_name")
+					.AttributeValue;
+				obj.GroupName += " " + Context.WebuserAttributes
+					.First(a => a.IdWebuser.ToString() == obj.GenericId && a.AttributeName == "last_name")
+					.AttributeValue; 
+			});
+
 			return permissionObjects;
+		}
+		
+		public Guid AddOrUpdate(PermissionObject permissionObject)
+		{
+			var isExistRecord = Context.PermissionObjects.Any(p => p.Id == permissionObject.Id);
+
+			if (isExistRecord)
+				Context.PermissionObjects.Update(permissionObject);
+			else
+				Context.PermissionObjects.Add(permissionObject);
+
+			Context.SaveChanges();
+			
+			return permissionObject.Id;
+		}
+		
+		public bool Remove(Guid id)
+		{
+			var isExistRecord = Context.PermissionObjects.Any(p => p.Id == id);
+
+			if (isExistRecord)
+			{
+				Context.PermissionObjects.Remove(Context.PermissionObjects.First(p => p.Id == id));
+				Context.SaveChanges();
+			}
+
+			return true;
 		}
     }
 }
