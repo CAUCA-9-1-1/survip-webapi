8d973383be9b9e709928af049f5cd559c03dbfb3
michael.jolin <michael.jolin@cauca.ca>
2018-09-11 17:00:59 -0400

    reafctor(#245): Create a BaseODataController

---
+++ Survi.Prevention.WebApi/Controllers/BaseODataController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.Base;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+	[Produces("application/json")]
+	public abstract class BaseODataController<TService, TModel> : ODataController
+		where TModel : BaseModel
+		where TService : BaseCrudService<TModel>
+	{
+		protected readonly TService Service;
+		protected Guid CurrentUserId => Guid.Parse(User.Claims.FirstOrDefault(claim => claim.Type == JwtRegisteredClaimNames.Sid)?.Value);
+		protected string CurrentUserName => User.Claims.FirstOrDefault(claim => claim.Type == JwtRegisteredClaimNames.UniqueName)?.Value;
+		
+		protected BaseODataController(TService service)
+		{
+			Service = service;
+		}
+
+		protected string ReadBody()
+		{
+			string body = "";
+			using (var reader = new StreamReader(Request.Body))
+				body = reader.ReadToEnd();
+
+			return body;
+		}
+	}
+}
