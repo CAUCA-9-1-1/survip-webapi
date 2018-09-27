1f2f7898808f43bd9dd976887a94df7ed6a05d1b
Unknown <syd_freak@hotmail.com>
2018-09-27 09:46:08 -0400

    Feat(#143) Move migration connectionString to the appsettings.json file.

--- Survi.Prevention.DataLayer/ManagementContextFactory.cs
+++ Survi.Prevention.DataLayer/ManagementContextFactory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Survi.Prevention.DataLayer
 {
@@ -8,9 +10,14 @@ namespace Survi.Prevention.DataLayer
 	{
 		public ManagementContext CreateDbContext(string[] args)
 		{
+			IConfiguration configuration = new ConfigurationBuilder()
+				.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+				.AddJsonFile("appsettings.json")
+				.Build();
+
 			var builder = new DbContextOptionsBuilder<ManagementContext>();
 			// Note that this connection string is solely used to create migrations stuff.  That's why it isn't part of a configuration file.
-			var connectionString = "[YOUR CONNECTION STRING FOR MIGRATION]"; //Configuration.GetConnectionString("SurviPreventionDatabase");
+			var connectionString = configuration.GetConnectionString("SurviPreventionDatabaseForMigration");
 			builder.UseNpgsql(connectionString, npgOptions =>
 			{
 				npgOptions.CommandTimeout((int) TimeSpan.FromMinutes(10).TotalSeconds);
