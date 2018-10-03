e104a036bac8c4f7bc4dbcf8c95fd34c5855219c
frederic.catellier <syd_freak@hotmail.com>
2018-10-03 16:35:21 -0400

    Feat(#270) Add Migration for new tables.

--- Survi.Prevention.DataLayer/Mapping/FireSafetyDepartmentConfigurationRiskLevelMapping.cs
+++ Survi.Prevention.DataLayer/Mapping/FireSafetyDepartmentConfigurationRiskLevelMapping.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Survi.Prevention.DataLayer.Mapping.Base;
 using Survi.Prevention.Models.FireSafetyDepartments;
@@ -10,6 +11,9 @@ namespace Survi.Prevention.DataLayer.Mapping
 		{
 			b.HasQueryFilter(m => m.IsActive);
 			b.HasKey(m => m.Id);
+			b.HasOne(m => m.RiskLevel).WithMany()
+				.HasForeignKey(m => m.IdRiskLevel)
+				.HasConstraintName("fk_ffdconfigrisklevel_risklevel_idrisklevel");
 		}
 	}
 }
