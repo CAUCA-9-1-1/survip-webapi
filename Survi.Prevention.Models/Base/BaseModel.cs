ed3aa25fc98ab87ca7d24fcbcc08ccbea541fb9e
Philippe Robert <philippe.robert@cauca.ca>
2018-10-18 15:00:05 -0400

    feat(#158) Add Modified information in baseModel

--- Survi.Prevention.Models/Base/BaseModel.cs
+++ Survi.Prevention.Models/Base/BaseModel.cs
@@ -7,5 +7,7 @@ namespace Survi.Prevention.Models.Base
 		public Guid Id { get; set; } = Guid.NewGuid();
 		public DateTime CreatedOn { get; set; } = DateTime.Now;
 		public bool IsActive { get; set; } = true;
+		public DateTime? LastModifiedOn {get; set;}
+		public Guid? IdWebUserLastModifiedBy { get; set;}
 	}
 }
