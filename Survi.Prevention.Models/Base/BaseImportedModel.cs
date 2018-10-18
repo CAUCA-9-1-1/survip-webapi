b6d226b0ba0a81c85d3acf90d00777d361fbe8b9
Philippe Robert <philippe.robert@cauca.ca>
2018-10-18 15:01:13 -0400

    feat(#158) Create abstract class for models that could be imported

---
+++ Survi.Prevention.Models/Base/BaseImportedModel.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+namespace Survi.Prevention.Models.Base
+{
+    public abstract class BaseImportedModel : BaseModel
+    {
+		public string IdExtern { get; set;}
+		public DateTime ImportedOn { get; set;}
+		public bool HasBeenModified { get; set;}
+    }
+}
