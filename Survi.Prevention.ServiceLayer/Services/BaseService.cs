2bb79e0a4e95bb97619a77eafa49e58abc9b4fff
Philippe Robert <philippe.robert@cauca.ca>
2018-10-19 11:25:37 -0400

    feat(#158) Add modification information in crud service

--- Survi.Prevention.ServiceLayer/Services/BaseService.cs
+++ Survi.Prevention.ServiceLayer/Services/BaseService.cs
@@ -6,7 +6,6 @@ namespace Survi.Prevention.ServiceLayer.Services
 	public abstract class BaseService
 	{
 		protected readonly ManagementContext Context;
-		public Guid idUserModified { get; set;}
 		protected BaseService(ManagementContext context)
 		{
 			Context = context;
