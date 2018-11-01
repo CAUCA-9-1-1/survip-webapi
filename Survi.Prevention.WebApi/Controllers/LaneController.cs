a570a6fc0d5bdb520027fa81d610088999ea999c
Unknown <syd_freak@hotmail.com>
2018-11-01 09:42:28 -0400

    Fix(#230) Allow slashes in searchTerms.

--- Survi.Prevention.WebApi/Controllers/LaneController.cs
+++ Survi.Prevention.WebApi/Controllers/LaneController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Survi.Prevention.Models.FireSafetyDepartments;
 using Survi.Prevention.ServiceLayer.Services;
@@ -31,10 +32,11 @@ namespace Survi.Prevention.WebApi.Controllers
 			return Ok(Service.GetListByCityLocalized(cityId, languageCode));
 		}
 
-	    [HttpGet, Route("city/{cityId:Guid}/Search/{searchTerm?}")]
+	    [HttpGet, Route("city/{cityId:Guid}/Search/{*searchTerm}")]
 	    public ActionResult GetFilteredLanesLocalized(Guid cityId, [FromHeader(Name = "Language-Code")]string languageCode, string searchTerm)
 	    {
-		    return Ok(Service.GetFilteredLanesLocalized(cityId, languageCode, searchTerm));
+		    searchTerm = HttpUtility.UrlDecode(searchTerm);
+			return Ok(Service.GetFilteredLanesLocalized(cityId, languageCode, searchTerm));
 	    }
 
         [HttpGet, Route("localized")]
