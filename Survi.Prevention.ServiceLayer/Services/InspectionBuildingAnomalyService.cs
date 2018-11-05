357a77bc1b0bbb3cb9ba8e6b040ba548dfa3d7dc
Philippe Robert <philippe.robert@cauca.ca>
2018-11-05 10:24:01 -0500

    feat(#240) Add validation on inspection anomaly picture delete

--- Survi.Prevention.ServiceLayer/Services/InspectionBuildingAnomalyService.cs
+++ Survi.Prevention.ServiceLayer/Services/InspectionBuildingAnomalyService.cs
@@ -8,55 +8,55 @@ using Survi.Prevention.Models.InspectionManagement.BuildingCopy;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
-    public class InspectionBuildingAnomalyService : BaseCrudService<InspectionBuildingAnomaly>
-    {
-	    public InspectionBuildingAnomalyService(ManagementContext context) : base(context)
-	    {
-	    }
+	public class InspectionBuildingAnomalyService : BaseCrudService<InspectionBuildingAnomaly>
+	{
+		public InspectionBuildingAnomalyService(ManagementContext context) : base(context)
+		{
+		}
 
-	    public override InspectionBuildingAnomaly Get(Guid id)
-	    {
-		    return Context.InspectionBuildingAnomalies.AsNoTracking()
-			    .FirstOrDefault(anomaly => anomaly.Id == id);
-	    }
+		public override InspectionBuildingAnomaly Get(Guid id)
+		{
+			return Context.InspectionBuildingAnomalies.AsNoTracking()
+				.FirstOrDefault(anomaly => anomaly.Id == id);
+		}
 
-	    public List<InspectionBuildingAnomaly> GetList(Guid idBuilding)
-	    {
-		    var value =  Context.InspectionBuildingAnomalies
+		public List<InspectionBuildingAnomaly> GetList(Guid idBuilding)
+		{
+			var value = Context.InspectionBuildingAnomalies
 				.Where(a => a.IsActive && a.IdBuilding == idBuilding)
-			    .Include(a => a.Pictures)
-			    .ThenInclude(p => p.Picture)
-			    .ToList();
+				.Include(a => a.Pictures)
+				.ThenInclude(p => p.Picture)
+				.ToList();
 
 			return value;
-	    }
-	    
-	    public List<InspectionBuildingAnomalyThemeForList> GetListForWeb(Guid idBuilding)
-	    {
-		    var query =
-			    from anomaly in Context.InspectionBuildingAnomalies.AsNoTracking()
-			    where anomaly.IdBuilding == idBuilding && anomaly.IsActive
-			    select new BuildingAnomalyForList
-			    {
+		}
+
+		public List<InspectionBuildingAnomalyThemeForList> GetListForWeb(Guid idBuilding)
+		{
+			var query =
+				from anomaly in Context.InspectionBuildingAnomalies.AsNoTracking()
+				where anomaly.IdBuilding == idBuilding && anomaly.IsActive
+				select new BuildingAnomalyForList
+				{
 					Id = anomaly.Id,
 					Theme = anomaly.Theme,
 					Notes = anomaly.Notes
-			    };
+				};
 
-		    var result = query.ToList();
+			var result = query.ToList();
 
-		    var finalResult = 
-			    from anomaly in result
-			    group anomaly by anomaly.Theme
-			    into theme
-			    select new InspectionBuildingAnomalyThemeForList
-			    {
+			var finalResult =
+				from anomaly in result
+				group anomaly by anomaly.Theme
+				into theme
+				select new InspectionBuildingAnomalyThemeForList
+				{
 					Theme = theme.Key,
 					Anomalies = theme.ToList()
-			    };
+				};
 
-		    return finalResult.ToList();
-	    }
+			return finalResult.ToList();
+		}
 
 		public List<string> GetThemes()
 		{
@@ -68,43 +68,45 @@ namespace Survi.Prevention.ServiceLayer.Services
 			return query.Distinct().ToList();
 		}
 
-	    public virtual Guid AddOrUpdatePicture(InspectionBuildingAnomalyPicture entity)
-	    {
-		    var isExistRecord = Context.InspectionBuildingAnomalyPictures.Any(c => c.Id == entity.Id);
+		public virtual Guid AddOrUpdatePicture(InspectionBuildingAnomalyPicture entity)
+		{
+			var isExistRecord = Context.InspectionBuildingAnomalyPictures.Any(c => c.Id == entity.Id);
 
-		    if (isExistRecord)
-			    Context.InspectionBuildingAnomalyPictures.Update(entity);
-		    else
-			    Context.InspectionBuildingAnomalyPictures.Add(entity);
+			if (isExistRecord)
+				Context.InspectionBuildingAnomalyPictures.Update(entity);
+			else
+				Context.InspectionBuildingAnomalyPictures.Add(entity);
 
-		    Context.SaveChanges();
-		    return entity.Id;
-	    }
+			Context.SaveChanges();
+			return entity.Id;
+		}
 
-	    public virtual bool Delete(Guid id)
-	    {
-		    var entity = Context.InspectionBuildingAnomalies.Find(id);
-		    if(entity != null){
+		public virtual bool Delete(Guid id)
+		{
+			var entity = Context.InspectionBuildingAnomalies.Find(id);
+			if (entity != null)
+			{
 				DeleteAnomalyPictures(entity.Id);
-			    Remove(id);
-			    Context.SaveChanges();
-			    return true;
-		    }
-	
-			return false;
-	    }
+				Remove(id);
+				Context.SaveChanges();
+				return true;
+			}
 
-	    private void DeleteAnomalyPictures(Guid idBuildingAnomaly)
-	    {
-		    var anomalyPictures = Context.InspectionBuildingAnomalyPictures
+			return false;
+		}
+
+		private void DeleteAnomalyPictures(Guid idBuildingAnomaly)
+		{
+			var anomalyPictures = Context.InspectionBuildingAnomalyPictures
 									.Where(bap => bap.IdBuildingAnomaly == idBuildingAnomaly)
 									.ToList();
-		    anomalyPictures.ForEach(pic =>
-		    {
-			    pic.IsActive = false;
-			    var picture = Context.InspectionPictures.Find(pic.IdPicture);
-			    picture.IsActive = false;
-		    });
-	    }
+			anomalyPictures.ForEach(pic =>
+			{
+				pic.IsActive = false;
+				var picture = Context.InspectionPictures.Find(pic.IdPicture);
+				if (picture != null)
+					picture.IsActive = false;
+			});
+		}
 	}
 }
