a7e31c286926ff6870a91d04b6c68c6395de4a4f
Philippe Robert <philippe.robert@cauca.ca>
2018-11-07 15:02:02 -0500

    feat(#361) Hot FIx from code review and unit test

---
+++ Survi.Prevention.Models/SurveyManagement/SurveyQuestionIdsConnector.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Survi.Prevention.Models.SurveyManagement
+{
+	public class SurveyQuestionIdsConnector
+	{
+		public Guid OriginalId { get; set; }
+		public Guid NewId { get; set; }
+	}
+}
