3e0f7a5063f5100adc38a94dca2320ea7d09858f
frederic.catellier <syd_freak@hotmail.com>
2018-11-13 09:19:58 -0500

    Feat(#159) Add API Client's partial unit testing.

---
+++ Survi.Prevention.ApiClient.Tests/Repositories/ExceptionsTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using Survi.Prevention.ApiClient.Exceptions;
+
+namespace Survi.Prevention.ApiClient.Tests.Repositories
+{
+    [TestFixture]
+    public class ExceptionsTests
+    {
+        private static string Url = "http://www.test.com/";
+
+        [TestCase]
+        public static void BadParameterApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API returned a 400 (bad request) response for url 'http://www.test.com/'.", new BadParameterApiException(Url).Message);
+        }
+
+        [TestCase]
+        public static void ExpiredRefreshTokenExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("The refresh token is expired.", new ExpiredRefreshTokenException().Message);
+        }
+
+        [TestCase]
+        public static void ForbiddenApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API returned a 403 (forbidden) response for url 'http://www.test.com/'.", new ForbiddenApiException(Url).Message);
+        }
+
+        [TestCase]
+        public static void InternalErrorApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API returned a 500 (internal error) response for url 'http://www.test.com/'.", new InternalErrorApiException(Url).Message);
+        }
+
+        [TestCase]
+        public static void InvalidRefreshTokenApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("The refresh token is invalid.", new InvalidRefreshTokenException().Message);
+        }
+
+        [TestCase]
+        public static void NoResponseApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API didn't return an answer in a timely manner.", new NoResponseApiException().Message);
+        }
+
+        [TestCase]
+        public static void NotFoundApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API returned a 404 (not found) response for url 'http://www.test.com/'.", new NotFoundApiException(Url).Message);
+        }
+
+        [TestCase]
+        public static void UnauthorizedApiExceptionMessageIsCorrectlyGenerated()
+        {
+            Assert.AreEqual("API returned a 401 (unauthorized) response for url 'http://www.test.com/'.", new UnauthorizedApiException(Url).Message);
+        }
+    }
+}
---
+++ Survi.Prevention.ApiClient.Tests/Repositories/RestResponseExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using RestSharp;
+using Survi.Prevention.ApiClient.Configurations;
+using Survi.Prevention.ApiClient.Services.Base;
+
+namespace Survi.Prevention.ApiClient.Tests.Repositories
+{
+    [TestFixture]
+    public class RestResponseExtensionsTests
+    {
+        private static IRestResponse<TokenRefreshResult> GetResponse(System.Net.HttpStatusCode code, string headerName)
+        {
+            var mockResponse = new Mock<IRestResponse<TokenRefreshResult>>();
+            mockResponse.Setup(res => res.StatusCode)
+                .Returns(code);
+            mockResponse.Setup(res => res.Headers)
+                .Returns(new List<Parameter> { new Parameter { Name = headerName } });
+            return mockResponse.Object;
+        }
+
+        [TestCase]
+        public void ExpiredRefreshTokenIsCorrectlyDetected()
+        {
+            var response = GetResponse(System.Net.HttpStatusCode.Unauthorized, RestResponseExtensions.RefreshTokenExpired);
+            Assert.IsTrue(response.RefreshTokenIsExpired());
+        }
+
+        [TestCase]
+        public void InvalidRefreshTokenIsCorrectlyDetected()
+        {
+            var response = GetResponse(System.Net.HttpStatusCode.Unauthorized, RestResponseExtensions.TokenInvalid);
+            Assert.IsTrue(response.RefreshTokenIsInvalid());
+        }
+
+        [TestCase]
+        public void InvalidAccessTokenIsCorrectlyDetected()
+        {
+            var response = GetResponse(System.Net.HttpStatusCode.Unauthorized, RestResponseExtensions.RefreshTokenExpired);
+            Assert.IsTrue(response.RefreshTokenIsExpired());
+        }
+    }
+}
