ff0643961e0d3b2bae325e45a5753269ebba20fb
Philippe Robert <philippe.robert@cauca.ca>
2018-11-15 08:55:45 -0500

    feat(#186) Add validation and unit test on languages

---
+++ Survi.Prevention.ServiceLayer/FormatFluentValidationErrorsToStringList.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation.Results;
+
+namespace Survi.Prevention.ServiceLayer
+{
+    public class FormatFluentValidationErrorsToStringList
+    {
+	    public List<string> GetFluentValidationErrorList(List<ValidationFailure> errorFailures)
+	    {
+			List<string> errorList = new List<string>();
+		    errorFailures.ForEach(error =>errorList.Add(error.ErrorCode+" | "+error.ErrorMessage));
+		    return errorList;
+	    }
+    }
+}
