32044e8048ab3c6046f756c7c875d4ec022e1dcd
frederic.catellier <syd_freak@hotmail.com>
2018-11-20 11:09:06 -0500

    Feat(#197) Add validators for new importation + refactoring existing validation code.

---
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/UtilisationCodeImportationValidator.cs
@@ -0,0 +1,13 @@
+﻿using Survi.Prevention.ServiceLayer.ValidationUtilities;
+
+namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation
+{
+    public class UtilisationCodeImportationValidator : BaseImportValidator<ApiClient.DataTransferObjects.UtilisationCode>
+    {
+        public UtilisationCodeImportationValidator()
+        {
+            RuleFor(m => m.Cubf).NotNullOrEmpty();
+            RuleFor(m => m.Scian).NotNullOrEmpty();
+        }
+    }
+}
