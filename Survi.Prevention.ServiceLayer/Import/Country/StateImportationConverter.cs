b47896562907683a0e21a815d009a9e5943e6909
frederic.catellier <syd_freak@hotmail.com>
2018-11-20 16:12:10 -0500

    Feat(#197) Refactor State Importation.

---
+++ Survi.Prevention.ServiceLayer/Import/Country/StateImportationConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using Survi.Prevention.DataLayer;
+using Survi.Prevention.Models.FireSafetyDepartments;
+using Survi.Prevention.ServiceLayer.Import.Base;
+using ImportedState = Survi.Prevention.ApiClient.DataTransferObjects.State;
+using State = Survi.Prevention.Models.FireSafetyDepartments.State;
+
+namespace Survi.Prevention.ServiceLayer.Import.Country
+{
+    public class StateImportationConverter
+        : BaseLocalizableEntityConverter<ImportedState, State, StateLocalization>
+    {
+        public StateImportationConverter(IManagementContext context, AbstractValidator<ImportedState> validator)
+            : base(context, validator)
+        {
+        }
+
+        protected override void CopyCustomFieldsToEntity(ImportedState importedObject, State entity)
+        {
+            entity.IdCountry = Guid.Parse(importedObject.IdCountry);
+            entity.AnsiCode = importedObject.AnsiCode;
+        }
+
+        protected override void GetRealForeignKeys(ImportedState importedObject)
+        {
+            var idCountry = Context.Set<Models.FireSafetyDepartments.Country>()
+                .FirstOrDefault(country => country.IdExtern == importedObject.IdCountry)?.Id;
+            importedObject.IdCountry = idCountry.HasValue ? idCountry.ToString() : null;
+        }
+    }
+}
