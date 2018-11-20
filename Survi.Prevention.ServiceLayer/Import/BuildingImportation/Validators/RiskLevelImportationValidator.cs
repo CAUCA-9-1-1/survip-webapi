a87dc2cd8356c31833d30be6a389bc3579945d30
Unknown <syd_freak@hotmail.com>
2018-11-20 15:52:20 -0500

    Feat(#197) Create a generic converter + giga refactoring.

--- Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/RiskLevelImportationValidator.cs
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/RiskLevelImportationValidator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using FluentValidation;
-
-namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation.Validators
+﻿namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation.Validators
 {
     public class RiskLevelImportationValidator 
         : BaseImportValidator<ApiClient.DataTransferObjects.RiskLevel>
