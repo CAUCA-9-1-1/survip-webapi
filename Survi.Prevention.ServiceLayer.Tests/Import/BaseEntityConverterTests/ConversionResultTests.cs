ff54537ec3ba7bce770ebea6f4afb6dc622b1e0a
frederic.catellier <syd_freak@hotmail.com>
2018-11-21 10:38:57 -0500

    Feat(#197) Add BaseEntityConverter unit tests.

---
+++ Survi.Prevention.ServiceLayer.Tests/Import/BaseEntityConverterTests/ConversionResultTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Survi.Prevention.ServiceLayer.Import.Base;
+using Xunit;
+
+namespace Survi.Prevention.ServiceLayer.Tests.Import.BaseEntityConverterTests
+{
+    public class ConversionResultTests
+    {
+        [Fact]
+        public void ConversionIsValidWhenThereIsNoValidationErrors()
+        {
+            var result = new ConversionResult<int> { ValidationErrors = new List<string>() };
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ConversionIsValidWhenValidationErrorsIsNull()
+        {
+            var result = new ConversionResult<int> { ValidationErrors = null };
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ConversionIsNotValidWhenThereIsNoValidationErrors()
+        {
+            var result = new ConversionResult<int> { ValidationErrors = new List<string> { "Hey!" } };
+            Assert.False(result.IsValid);
+        }
+    }
+}
---
+++ Survi.Prevention.ServiceLayer.Tests/Import/BaseEntityConverterTests/Mocks/BaseEntityValidatorMock.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Survi.Prevention.ServiceLayer.Tests.Import.BaseEntityConverterTests.Mocks
+{
+    public class BaseEntityValidatorMock : AbstractValidator<ApiClient.DataTransferObjects.Country>
+    {
+        public BaseEntityValidatorMock()
+        {
+            RuleFor(m => m.CodeAlpha2).NotNull();
+        }
+    }
+}
