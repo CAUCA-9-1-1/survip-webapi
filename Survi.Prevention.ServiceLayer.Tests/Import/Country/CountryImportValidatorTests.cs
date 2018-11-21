82655d56c7f8a9d6122ff283b66993a7960fa192
frederic.catellier <syd_freak@hotmail.com>
2018-11-21 08:06:12 -0500

    Feat(#197) country & state's validators unit tests refactored.

--- Survi.Prevention.ServiceLayer.Tests/Import/Country/CountryImportValidatorTests.cs
+++ Survi.Prevention.ServiceLayer.Tests/Import/Country/CountryImportValidatorTests.cs
@@ -1,53 +1,63 @@
-﻿using System.Collections.Generic;
-using Survi.Prevention.ServiceLayer.Import.Country;
+﻿using Survi.Prevention.ServiceLayer.Import.Country;
 using Xunit;
-using countryImported = Survi.Prevention.ApiClient.DataTransferObjects;
+using FluentValidation.TestHelper;
 
 namespace Survi.Prevention.ServiceLayer.Tests.Import.Country
 {
     public class CountryImportValidatorTests
     {
-	    private readonly countryImported.Country importedCountry;
 	    private readonly CountryValidator validator;
 
 	    public CountryImportValidatorTests()
 	    {
 			validator = new CountryValidator();
-		    importedCountry = new countryImported.Country
-		    {
-			    Id = "country1",
-			    CodeAlpha2 = "CO",
-			    CodeAlpha3 = "CO3",
-			    IsActive = true,
-			    Localizations = new List<countryImported.Base.Localization>
-			    {
-				    new countryImported.Base.Localization{Name = "Country 1", LanguageCode = "en"},
-				    new countryImported.Base.Localization{Name = "Pays 1", LanguageCode = "fr"}
-			    }
-		    };
 	    }
 
 	    [Fact]
-	    public void CountryIsValid()
+        public void IdCountryIsValidWhenItHasAValue()
 	    {
-		    var result = validator.Validate(importedCountry);
-		    Assert.True(result.IsValid);
+	        validator.ShouldNotHaveValidationErrorFor(country => country.Id, "country008");
 	    }
 
-	    [Fact]
-	    public void CountryMustContainsValidCodeAlpha2()
-	    {
-		    importedCountry.CodeAlpha2 = "test";
-		    var result = validator.Validate(importedCountry);
-		    Assert.False(result.IsValid);
-	    }
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void IdCountryIsInvalidWhenNullEmptyOrTooLong(string idCountry)
+        {
+            validator.ShouldHaveValidationErrorFor(country => country.Id, idCountry);
+        }
 
-	    [Fact]
-	    public void CountryMustContainsValidCodeAlpha3()
-	    {
-		    importedCountry.CodeAlpha3 = "test";
-		    var result = validator.Validate(importedCountry);
-		    Assert.False(result.IsValid);
-	    }
+        [Fact]
+        public void AlphaCode2IsValidWhenItHasAValue()
+        {
+            validator.ShouldNotHaveValidationErrorFor(country => country.CodeAlpha2, "QC");
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("WayTooLongCode")]
+        [InlineData(null)]
+        public void AlphaCode2IsInvalidWhenNullOrEmpty(string alphaCode2)
+        {
+            validator.ShouldHaveValidationErrorFor(country => country.CodeAlpha2, alphaCode2);
+        }
+
+        [Fact]
+        public void AlphaCode3IsValidWhenItHasAValue()
+        {
+            validator.ShouldNotHaveValidationErrorFor(country => country.CodeAlpha3, "QCB");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("WayTooLongCode")]
+        [InlineData(null)]
+        public void AlphaCode3IsInvalidWhenNullOrEmpty(string alphaCode2)
+        {
+            validator.ShouldHaveValidationErrorFor(country => country.CodeAlpha3, alphaCode2);
+        }
     }
 }
