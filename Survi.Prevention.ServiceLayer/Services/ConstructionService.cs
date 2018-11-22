a8d7bfbe90dae1bd184fa87059fda0ce90521f88
frederic.catellier <syd_freak@hotmail.com>
2018-11-22 09:18:50 -0500

    Feat(#199) Make construction service + controller compatible with importation of construction types.

--- Survi.Prevention.ServiceLayer/Services/ConstructionService.cs
+++ Survi.Prevention.ServiceLayer/Services/ConstructionService.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Survi.Prevention.ApiClient.Configurations;
+using Survi.Prevention.ApiClient.DataTransferObjects;
+using Survi.Prevention.ApiClient.DataTransferObjects.Base;
 using Survi.Prevention.DataLayer;
+using Survi.Prevention.Models.Base;
 using Survi.Prevention.Models.DataTransfertObjects;
+using Survi.Prevention.ServiceLayer.Import.Base;
+using ImportConstructionType = Survi.Prevention.ApiClient.DataTransferObjects.ConstructionType;
+using ImportFireResistanceType = Survi.Prevention.ApiClient.DataTransferObjects.ConstructionFireResistanceType;
+using ImportSidingType = Survi.Prevention.ApiClient.DataTransferObjects.SidingType;
+using ImportRoofType = Survi.Prevention.ApiClient.DataTransferObjects.RoofType;
+using ImportRoofMaterialType = Survi.Prevention.ApiClient.DataTransferObjects.RoofMaterialType;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
 	public class ConstructionService : BaseService
 	{
-		public ConstructionService(IManagementContext context) : base(context)
+	    private readonly List<object> converters = new List<object>();
+
+		public ConstructionService(
+		    IManagementContext context,
+		    IEntityConverter<ImportConstructionType, ConstructionType> constructionTypeConverter,
+		    IEntityConverter<ImportFireResistanceType, ConstructionFireResistanceType> fireResistanceTypeConverter,
+	        IEntityConverter<ImportSidingType, SidingType> sidingTypeConverter,
+	        IEntityConverter<ImportRoofType, RoofType> roofTypeConverter,
+	        IEntityConverter<ImportRoofMaterialType, RoofMaterialType> roofMaterialTypeConverter
+        ) : base(context)
 		{
-		}
+		    converters.Add(constructionTypeConverter);
+		    converters.Add(fireResistanceTypeConverter);
+		    converters.Add(sidingTypeConverter);
+		    converters.Add(roofTypeConverter);
+		    converters.Add(roofMaterialTypeConverter);
+        }
 
 		public List<GenericModelForDisplay> GetBuildingSidingTypes(string languageCode)
 		{
@@ -96,5 +120,43 @@ namespace Survi.Prevention.ServiceLayer.Services
 				RoofTypes = GetRoofTypes(languageCode)
 			};
 		}
-	}
+
+	    public List<ImportationResult> Import<TEntity,TImportedEntity>(List<TImportedEntity> entities)
+	        where TEntity : BaseImportedModel, new()
+	        where TImportedEntity : BaseTransferObject
+        {
+	        var resultList = new List<ImportationResult>();
+	        foreach (var entity in entities)
+	            resultList.Add(Import<TEntity, TImportedEntity>(entity));
+
+	        return resultList;
+	    }
+
+	    protected ImportationResult Import<TEntity, TImportedEntity>(TImportedEntity importedEntity)
+	        where TEntity : BaseImportedModel, new()
+	        where TImportedEntity : BaseTransferObject
+        {
+	        var result = GetImportationResult<TEntity, TImportedEntity>(importedEntity);
+
+	        if (result.IsValid)
+	        {
+	            Context.SaveChanges();
+	            result.HasBeenImported = true;
+	        }
+	        return result;
+	    }
+
+	    protected ImportationResult GetImportationResult<TEntity, TImportedEntity>(TImportedEntity importedEntity)
+	        where TEntity : BaseImportedModel, new()
+	        where TImportedEntity : BaseTransferObject
+	    {
+	        var converter = converters.OfType<IEntityConverter<TImportedEntity, TEntity>>().First();
+            var conversionResult = converter.Convert(importedEntity);
+	        return new ImportationResult
+	        {
+	            IdEntity = importedEntity.Id,
+	            Messages = conversionResult.ValidationErrors
+	        };
+	    }
+    }
 }
--- Survi.Prevention.WebApi/Controllers/ConstructionController.cs
+++ Survi.Prevention.WebApi/Controllers/ConstructionController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.Buildings;
 using Survi.Prevention.ServiceLayer.Services;
+using ImportConstructionType = Survi.Prevention.ApiClient.DataTransferObjects.ConstructionType;
+using ImportFireResistanceType = Survi.Prevention.ApiClient.DataTransferObjects.ConstructionFireResistanceType;
+using ImportSidingType = Survi.Prevention.ApiClient.DataTransferObjects.SidingType;
+using ImportRoofType = Survi.Prevention.ApiClient.DataTransferObjects.RoofType;
+using ImportRoofMaterialType = Survi.Prevention.ApiClient.DataTransferObjects.RoofMaterialType;
 
 namespace Survi.Prevention.WebApi.Controllers
 {
@@ -54,5 +62,35 @@ namespace Survi.Prevention.WebApi.Controllers
 		{
 			return Ok(service.GetAllTypes(languageCode));
 		}
-	}
+
+	    [HttpPost, Route("ConstructionType/Import"), AllowAnonymous]
+	    public ActionResult ImportConstructionTypes([FromBody] List<ImportConstructionType> importedEntities)
+	    {
+	        return Ok(service.Import<ConstructionType, ImportConstructionType>(importedEntities));
+	    }
+
+	    [HttpPost, Route("ConstructionFireResistanceType/Import"), AllowAnonymous]
+	    public ActionResult ImportConstructionFireResistanceTypes([FromBody] List<ImportFireResistanceType> importedEntities)
+	    {
+	        return Ok(service.Import<ConstructionFireResistanceType, ImportFireResistanceType>(importedEntities));
+	    }
+
+	    [HttpPost, Route("SidingType/Import"), AllowAnonymous]
+	    public ActionResult ImportSidingTypes([FromBody] List<ImportSidingType> importedEntities)
+	    {
+	        return Ok(service.Import<SidingType, ImportSidingType>(importedEntities));
+        }
+
+	    [HttpPost, Route("RoofType/Import"), AllowAnonymous]
+	    public ActionResult ImportRoofTypes([FromBody] List<ImportRoofType> importedEntities)
+	    {
+	        return Ok(service.Import<RoofType, ImportRoofType>(importedEntities));
+        }
+
+	    [HttpPost, Route("RoofMaterialType/Import"), AllowAnonymous]
+	    public ActionResult ImportRoofMaterialTypes([FromBody] List<ImportRoofMaterialType> importedEntities)
+	    {
+	        return Ok(service.Import<RoofMaterialType, ImportRoofMaterialType>(importedEntities));
+        }
+    }
 }
