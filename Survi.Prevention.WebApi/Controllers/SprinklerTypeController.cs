c71dac4d99ad37f665c4a74bda606219e8a93492
frederic.catellier <syd_freak@hotmail.com>
2018-11-22 08:50:06 -0500

    Feat(#199) alarm panel types + sprinklers controllers now accept importation.

--- Survi.Prevention.WebApi/Controllers/SprinklerTypeController.cs
+++ Survi.Prevention.WebApi/Controllers/SprinklerTypeController.cs
@@ -1,22 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.Buildings;
 using Survi.Prevention.ServiceLayer.Services;
 
 namespace Survi.Prevention.WebApi.Controllers
 {
 	[Route("api/SprinklerType")]
-	public class SprinklerTypeController : BaseSecuredController
-	{
-		private readonly SprinklerTypeService service;
-
-		public SprinklerTypeController(SprinklerTypeService service)
+	public class SprinklerTypeController 
+	    : BaseCrudControllerWithImportation<SprinklerTypeService, SprinklerType, ApiClient.DataTransferObjects.SprinklerType>
+    {
+		public SprinklerTypeController(SprinklerTypeService service) : base(service)
 		{
-			this.service = service;
 		}
 
 		[HttpGet]
 		public ActionResult GetList([FromHeader(Name = "Language-Code")]string languageCode)
 		{
-			return Ok(service.GetList(languageCode));
+			return Ok(Service.GetList(languageCode));
 		}
 	}
 }
