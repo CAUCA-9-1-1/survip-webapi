016a47a402f49a48aa05002452d3b168d1401c34
frederic.catellier <syd_freak@hotmail.com>
2018-11-22 16:53:38 -0500

    Feat(#196) Add hydrant's validator, converter + unit tests.

---
+++ Survi.Prevention.ServiceLayer.Tests/Import/FireHydrantImportation/Mocks/UnitOfMeasureImportationConverterMock.cs
@@ -0,0 +1,17 @@
+﻿using Survi.Prevention.ApiClient.DataTransferObjects;
+using Survi.Prevention.ServiceLayer.Import.FireHydrantImportation;
+
+namespace Survi.Prevention.ServiceLayer.Tests.Import.FireHydrantImportation.Mocks
+{
+    public class UnitOfMeasureImportationConverterMock : UnitOfMeasureImportationConverter
+    {
+        public UnitOfMeasureImportationConverterMock() : base(null, null)
+        {
+        }
+
+        public void CopyCustomFields(UnitOfMeasure imported, Models.UnitOfMeasure entity)
+        {
+            CopyCustomFieldsToEntity(imported, entity);
+        }
+    }
+}
---
+++ Survi.Prevention.ServiceLayer/Import/FireHydrantImportation/UnitOfMeasureImportationConverter.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Survi.Prevention.ApiClient.DataTransferObjects;
+using Survi.Prevention.DataLayer;
+using Survi.Prevention.ServiceLayer.Import.Base;
+
+namespace Survi.Prevention.ServiceLayer.Import.FireHydrantImportation
+{
+    public class UnitOfMeasureImportationConverter 
+        : BaseLocalizableEntityConverter<UnitOfMeasure, Models.UnitOfMeasure, Models.UnitOfMeasureLocalization>
+    {
+        public UnitOfMeasureImportationConverter(IManagementContext context, AbstractValidator<UnitOfMeasure> validator) 
+            : base(context, validator)
+        {
+        }
+
+        protected override void CopyCustomFieldsToEntity(UnitOfMeasure importedObject, Models.UnitOfMeasure entity)
+        {
+            entity.Abbreviation = importedObject.Abbreviation;
+            entity.MeasureType = (Models.MeasureType) importedObject.MeasureType;
+        }
+    }
+}
