48b59fcde2023ee78b1b96d518551ad21b1e15fc
frederic.catellier <syd_freak@hotmail.com>
2018-11-23 12:13:55 -0500

    Feat(#196) Add migration for missing importation's fields.

--- Survi.Prevention.DataLayer/Migrations/20181123171310_Issue196-MissingImportationFields.Designer.cs
+++ Survi.Prevention.DataLayer/Migrations/20181123171310_Issue196-MissingImportationFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -10,9 +11,10 @@ using Survi.Prevention.DataLayer;
 namespace Survi.Prevention.DataLayer.Migrations
 {
     [DbContext(typeof(ManagementContext))]
-    partial class ManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20181123171310_Issue196-MissingImportationFields")]
+    partial class Issue196MissingImportationFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Survi.Prevention.DataLayer/Migrations/20181123171310_Issue196-MissingImportationFields.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Survi.Prevention.DataLayer.Migrations
+{
+    public partial class Issue196MissingImportationFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "has_been_modified",
+                table: "fire_hydrant_connection_type",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "id_extern",
+                table: "fire_hydrant_connection_type",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "imported_on",
+                table: "fire_hydrant_connection_type",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "has_been_modified",
+                table: "fire_hydrant_connection",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "id_extern",
+                table: "fire_hydrant_connection",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "imported_on",
+                table: "fire_hydrant_connection",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "has_been_modified",
+                table: "fire_hydrant_connection_type");
+
+            migrationBuilder.DropColumn(
+                name: "id_extern",
+                table: "fire_hydrant_connection_type");
+
+            migrationBuilder.DropColumn(
+                name: "imported_on",
+                table: "fire_hydrant_connection_type");
+
+            migrationBuilder.DropColumn(
+                name: "has_been_modified",
+                table: "fire_hydrant_connection");
+
+            migrationBuilder.DropColumn(
+                name: "id_extern",
+                table: "fire_hydrant_connection");
+
+            migrationBuilder.DropColumn(
+                name: "imported_on",
+                table: "fire_hydrant_connection");
+        }
+    }
+}
