3d60821d74f1f0360fe70bbe239053df1305f503
Philippe Robert <philippe.robert@cauca.ca>
2018-11-26 14:28:35 -0500

    feat(#198) Implement buildingHazardousMaterial import and testing

---
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/BuildingHazardousMaterialImportationConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentValidation;
+using Survi.Prevention.DataLayer;
+using Survi.Prevention.Models;
+using Survi.Prevention.Models.Buildings;
+using Survi.Prevention.ServiceLayer.Import.Base;
+using importedBuildingHazardousMaterial = Survi.Prevention.ApiClient.DataTransferObjects.BuildingHazardousMaterial;
+
+namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation
+{
+	public class BuildingHazardousMaterialImportationConverter : BaseEntityConverter<importedBuildingHazardousMaterial, BuildingHazardousMaterial>
+	{
+		public BuildingHazardousMaterialImportationConverter(IManagementContext context, AbstractValidator<importedBuildingHazardousMaterial> validator) : base(context, validator)
+		{
+		}
+
+		protected override void CopyCustomFieldsToEntity(importedBuildingHazardousMaterial importedObject, BuildingHazardousMaterial entity)
+		{
+			entity.IdBuilding = Guid.Parse(importedObject.IdBuilding);
+			entity.IdHazardousMaterial = Guid.Parse(importedObject.IdHazardousMaterial);
+			entity.IdUnitOfMeasure = Guid.Parse(importedObject.IdUnitOfMeasure);
+
+			entity.CapacityContainer = importedObject.CapacityContainer;
+			entity.Container = importedObject.Container;
+			entity.Floor = importedObject.Floor;
+			entity.GasInlet = importedObject.GasInlet;
+			entity.OtherInformation = importedObject.OtherInformation;
+			entity.Place = importedObject.Place;
+			entity.Quantity = importedObject.Quantity;
+			entity.Sector = importedObject.Sector;
+			entity.SecurityPerimeter = importedObject.SecurityPerimeter;
+			entity.SupplyLine = importedObject.SupplyLine;
+			entity.TankType = (StorageTankType)importedObject.TankType;
+			entity.Wall = importedObject.Wall;
+		}
+
+		protected override void GetRealForeignKeys(importedBuildingHazardousMaterial importedObject)
+		{
+			importedObject.IdBuilding = GetRealId<Building>(importedObject.IdBuilding);
+			importedObject.IdHazardousMaterial = GetRealId<HazardousMaterial>(importedObject.IdHazardousMaterial);
+			importedObject.IdUnitOfMeasure = GetRealId<UnitOfMeasure>(importedObject.IdUnitOfMeasure);
+		}
+	}
+}
---
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/BuildingHazardousMaterialImportationValidator.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using Survi.Prevention.ApiClient.DataTransferObjects;
+using Survi.Prevention.ServiceLayer.ValidationUtilities;
+
+namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation.Validators
+{
+	public class BuildingHazardousMaterialImportationValidator : AbstractValidator<BuildingHazardousMaterial>
+	{
+		public BuildingHazardousMaterialImportationValidator()
+		{
+			RuleFor(m => m.Id)
+				.NotNullOrEmpty();
+
+			RuleFor(m => m.IdBuilding)
+				.NotNullOrEmpty();
+
+			RuleFor(m => m.IdHazardousMaterial)
+				.NotNullOrEmpty();
+
+			RuleFor(m => m.IdUnitOfMeasure)
+				.NotNullOrEmpty()
+				.When(IsContainerCapacityImplemented);
+			
+			RuleFor(m => m.CapacityContainer)
+				.GreaterThanOrEqualTo(0).WithMessage("{PropertyName}_InferiorToZeroValue");
+
+			RuleFor(m => m.Quantity)
+				.GreaterThanOrEqualTo(0).WithMessage("{PropertyName}_InferiorToZeroValue");
+
+			RuleFor(m => m.TankType)
+				.IsInEnum();
+
+			RuleFor(m => m.Container)
+				.NotNullMaxLength(100);
+
+			RuleFor(m => m.Floor)
+				.NotNullMaxLength(4);
+
+			RuleFor(m => m.GasInlet)
+				.NotNullMaxLength(100);
+
+			RuleFor(m => m.Place)
+				.NotNullMaxLength(150);
+
+			RuleFor(m => m.Sector)
+				.NotNullMaxLength(15);
+
+			RuleFor(m => m.SupplyLine)
+				.NotNullMaxLength(50);
+
+			RuleFor(m => m.SecurityPerimeter)
+				.NotNull().WithMessage("{PropertyName}_NullValue");
+
+			RuleFor(m => m.OtherInformation)
+				.NotNull().WithMessage("{PropertyName}_NullValue");
+
+			RuleFor(m => m.SupplyLine)
+				.NotNullMaxLength(50);
+
+			RuleFor(m => m.Wall)
+				.NotNullMaxLength(15);            
+		}
+
+		private bool IsContainerCapacityImplemented(BuildingHazardousMaterial arg) => arg.CapacityContainer > 0;
+	}
+}
