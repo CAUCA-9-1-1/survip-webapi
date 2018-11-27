005255fabad45f3d114cedc70167c8bf2a71d944
frederic.catellier <syd_freak@hotmail.com>
2018-11-27 08:03:52 -0500

    Feat(#200) Move BuildingParticularRisk validator to its own file.

---
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/BuildingParticularRiskImportationValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentValidation;
+using Survi.Prevention.ApiClient.DataTransferObjects;
+using Survi.Prevention.ServiceLayer.ValidationUtilities;
+
+namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation.Validators
+{
+    public class BuildingParticularRiskImportationValidator :
+        AbstractValidator<BuildingParticularRisk>
+    {
+        public BuildingParticularRiskImportationValidator()
+        {
+            RuleFor(m => m.RiskType)
+                .Must(type => Enum.IsDefined(typeof(ParticularRiskType), type))
+                .WithMessage("{PropertyName}_InvalidValue");
+            RuleFor(m => m.IdBuilding)
+                .RequiredKeyIsValid();
+
+            RuleFor(m => m.Dimension)
+                .MaxLength(100);
+            RuleFor(m => m.Wall)
+                .MaxLength(15);
+            RuleFor(m => m.Sector)
+                .MaxLength(15);
+        }
+    }
+}
--- Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/RiskLevelImportationValidator.cs
+++ Survi.Prevention.ServiceLayer/Import/BuildingImportation/Validators/RiskLevelImportationValidator.cs
@@ -1,32 +1,9 @@
-﻿using System;
-using FluentValidation;
-using Survi.Prevention.ApiClient.DataTransferObjects;
-using Survi.Prevention.ServiceLayer.ValidationUtilities;
+﻿using Survi.Prevention.ApiClient.DataTransferObjects;
 
 namespace Survi.Prevention.ServiceLayer.Import.BuildingImportation.Validators
 {
     public class RiskLevelImportationValidator
         : BaseImportValidator<RiskLevel>
     {
-    }
-
-    public class BuildingParticularRiskImportationValidator :
-        AbstractValidator<BuildingParticularRisk>
-    {
-        public BuildingParticularRiskImportationValidator()
-        {
-            RuleFor(m => m.RiskType)
-                .Must(type => Enum.IsDefined(typeof(ParticularRiskType), type))
-                .WithMessage("{PropertyName}_InvalidValue");
-            RuleFor(m => m.IdBuilding)
-                .RequiredKeyIsValid();
-
-            RuleFor(m => m.Dimension)
-                .MaxLength(100);
-            RuleFor(m => m.Wall)
-                .MaxLength(15);
-            RuleFor(m => m.Sector)
-                .MaxLength(15);
-        }
     }
 }
