51d4bf7262b1927cdde355cd7b8888236907306b
Philippe Robert <philippe.robert@cauca.ca>
2018-11-28 16:08:43 -0500

    feat(#198) : update size of wall and sector in building and Inspection HM

--- Survi.Prevention.DataLayer/Mapping/BuildingHazardousMaterialMapping.cs
+++ Survi.Prevention.DataLayer/Mapping/BuildingHazardousMaterialMapping.cs
@@ -14,9 +14,9 @@ namespace Survi.Prevention.DataLayer.Mapping
 			b.Property(m => m.Floor).HasMaxLength(4).IsRequired();
 			b.Property(m => m.Place).HasMaxLength(150).IsRequired();
 			b.Property(m => m.GasInlet).HasMaxLength(100).IsRequired();
-			b.Property(m => m.Wall).HasMaxLength(15).IsRequired();
+			b.Property(m => m.Wall).HasMaxLength(3).IsRequired();
 			b.Property(m => m.SupplyLine).HasMaxLength(50).IsRequired();
-			b.Property(m => m.Sector).HasMaxLength(15).IsRequired();
+			b.Property(m => m.Sector).HasMaxLength(3).IsRequired();
 			b.Property(m => m.SecurityPerimeter).IsRequired();
 			b.Property(m => m.OtherInformation).IsRequired();
 			b.Property(m => m.TankType).IsRequired();
---
+++ Survi.Prevention.DataLayer/Migrations/20181128210709_hazardous-wall-sector-max-length.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Survi.Prevention.DataLayer.Migrations
+{
+    public partial class hazardouswallsectormaxlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "wall",
+                table: "inspection_building_hazardous_material",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sector",
+                table: "inspection_building_hazardous_material",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "wall",
+                table: "building_hazardous_material",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sector",
+                table: "building_hazardous_material",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 15);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "wall",
+                table: "inspection_building_hazardous_material",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sector",
+                table: "inspection_building_hazardous_material",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "wall",
+                table: "building_hazardous_material",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sector",
+                table: "building_hazardous_material",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+        }
+    }
+}
