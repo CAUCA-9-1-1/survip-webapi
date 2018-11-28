f3bd1e14eb4ae9919c42decd6e19d426c58218e1
Philippe Robert <philippe.robert@cauca.ca>
2018-11-28 15:54:28 -0500

    feat(#187) Remove comments

--- Survi.Prevention.ApiClient/Configurations/Configuration.cs
+++ Survi.Prevention.ApiClient/Configurations/Configuration.cs
@@ -9,18 +9,4 @@
         string AccessToken { get; set; }
         string RefreshToken { get; set; }
     }
-
-    /*public class Configuration
-    {
-        private static Configuration currentConfiguration;
-        public static Configuration Current => currentConfiguration ?? (currentConfiguration = new Configuration());
-
-        public string ApiBaseUrl { get; set; }
-        public LoginInfo LoginInfo { get; set; }
-
-        public static void ResetConfiguration()
-        {
-            currentConfiguration = null;
-        }
-    }*/
 }
--- Survi.Prevention.ApiClient/Configurations/LoginResult.cs
+++ Survi.Prevention.ApiClient/Configurations/LoginResult.cs
@@ -5,12 +5,5 @@
         public string AuthorizationType { get; set; }
         public string TokenForAccess { get; set; }
         public string RefreshToken { get; set; }
-        /*public string IdWeb
-        token.ExpiresIn,
-        AccessToken = token.TokenForAccess,
-        token.RefreshToken,
-        IdWebuser = user.Id,
-        LastName = user.Attributes.Where(a => a.AttributeName=="last_name").Select(a => a.AttributeValue).FirstOrDefault() ?? "",
-        FirstName = user.Attributes.Where(a => a.AttributeName == "first_name").Select(a => a.AttributeValue).FirstOrDefault() ?? "",*/
     }
 }
