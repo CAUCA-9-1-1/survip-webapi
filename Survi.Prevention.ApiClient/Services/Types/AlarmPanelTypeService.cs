6a0e16503d6229e50722344f70c9b271b90b1283
frederic.catellier <syd_freak@hotmail.com>
2018-11-29 12:25:02 -0500

    Feat(#223) Add missing constructor

--- Survi.Prevention.ApiClient/Services/Types/AlarmPanelTypeService.cs
+++ Survi.Prevention.ApiClient/Services/Types/AlarmPanelTypeService.cs
@@ -1,4 +1,5 @@
-﻿using Survi.Prevention.ApiClient.DataTransferObjects;
+﻿using Survi.Prevention.ApiClient.Configurations;
+using Survi.Prevention.ApiClient.DataTransferObjects;
 using Survi.Prevention.ApiClient.Services.Base;
 
 namespace Survi.Prevention.ApiClient.Services.Types
@@ -6,5 +7,9 @@ namespace Survi.Prevention.ApiClient.Services.Types
 	public class AlarmPanelTypeService : BaseSecureService<AlarmPanelType>
 	{
 		protected override string BaseUrl { get; set; } = "AlarmPanelType/Import";
+
+	    public AlarmPanelTypeService(IConfiguration configuration) : base(configuration)
+	    {
+	    }
 	}    
 }
--- Survi.Prevention.ApiClient/Services/Types/SidingTypeService.cs
+++ Survi.Prevention.ApiClient/Services/Types/SidingTypeService.cs
@@ -1,4 +1,5 @@
-﻿using Survi.Prevention.ApiClient.DataTransferObjects;
+﻿using Survi.Prevention.ApiClient.Configurations;
+using Survi.Prevention.ApiClient.DataTransferObjects;
 using Survi.Prevention.ApiClient.Services.Base;
 
 namespace Survi.Prevention.ApiClient.Services.Types
@@ -6,5 +7,9 @@ namespace Survi.Prevention.ApiClient.Services.Types
 	public class SidingTypeService : BaseSecureService<SidingType>
 	{
 		protected override string BaseUrl { get; set; } = "Construction/SidingType/Import";
+
+	    public SidingTypeService(IConfiguration configuration) : base(configuration)
+	    {
+	    }
 	}    
 }
