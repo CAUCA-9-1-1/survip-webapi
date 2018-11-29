3d193490a06ed53978d9c08e58635e9860606372
frederic.catellier <syd_freak@hotmail.com>
2018-11-29 11:44:16 -0500

    Feat(#215) Add missing service.

---
+++ Survi.Prevention.ApiClient/Services/Building/BuildingFireHydrantService.cs
@@ -0,0 +1,14 @@
+﻿using Survi.Prevention.ApiClient.Configurations;
+using Survi.Prevention.ApiClient.Services.Base;
+
+namespace Survi.Prevention.ApiClient.Services.Building
+{
+    public class BuildingFireHydrantService : BaseSecureService<DataTransferObjects.BuildingFireHydrant>
+    {
+        protected override string BaseUrl { get; set; } = "Building/FireHydrant/Import";
+
+        public BuildingFireHydrantService(IConfiguration configuration) : base(configuration)
+        {
+        }        
+    }
+}
