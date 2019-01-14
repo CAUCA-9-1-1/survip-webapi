f4dd6f1f5c7c1bbf96e0c5bafa59a8373a0ceffa
Frederic Catellier <syd_freak@hotmail.com>
2019-01-14 11:17:57 -0500

    Feat(#301) Add a query to get building's list for an inspection.

---
+++ Survi.Prevention.Models/DataTransfertObjects/Inspections/InspectionWithBuilding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Survi.Prevention.Models.DataTransfertObjects.Inspections
+{
+    public class InspectionWithBuildings
+    {
+        public Guid Id { get; set; }
+        public List<InspectionBuildingResume> Buildings { get; set; }
+    }
+
+    public class InspectionBuildingResume
+    {
+        public Guid IdBuilding { get; set; }
+        public string Name { get; set; }
+        public bool IsMainBuilding { get; set; }
+    }
+}
