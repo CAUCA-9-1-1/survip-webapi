913a26b6d9cc439afa54d201058e43accf7f9dde
Philippe Robert <philippe.robert@cauca.ca>
2019-01-18 10:19:41 -0500

    feat(#243): Add picture to firesafetydepartment table

--- Survi.Prevention.Models/FireSafetyDepartments/FireSafetyDepartment.cs
+++ Survi.Prevention.Models/FireSafetyDepartments/FireSafetyDepartment.cs
@@ -8,10 +8,12 @@ namespace Survi.Prevention.Models.FireSafetyDepartments
 	{
 		public string Language { get; set; }
 		public Guid IdCounty { get; set; }
+        public Guid? IdPicture { get; set; }
 
-		public County County { get; set; }
+        public County County { get; set; }
+        public Picture Picture { get; set; }
 
-		public ICollection<FireSafetyDepartmentCityServing> FireSafetyDepartmentServing { get; set; }
+        public ICollection<FireSafetyDepartmentCityServing> FireSafetyDepartmentServing { get; set; }
 		public ICollection<Firestation> Firestations { get; set; }
 	}
 }
