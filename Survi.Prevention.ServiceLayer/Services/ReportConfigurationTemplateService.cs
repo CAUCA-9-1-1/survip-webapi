30ff9cd9c429684a8f49fe9e2b0009d4138d7535
audrey <audrey.lebelr@gmail.com>
2019-01-20 22:31:32 -0500

    fix(#257): White spaces

--- Survi.Prevention.ServiceLayer/Services/ReportConfigurationTemplateService.cs
+++ Survi.Prevention.ServiceLayer/Services/ReportConfigurationTemplateService.cs
@@ -8,44 +8,44 @@ using Survi.Prevention.ServiceLayer.DataCopy;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
-    public class ReportConfigurationTemplateService : BaseCrudService<ReportConfigurationTemplate>
-    {
-        public ReportConfigurationTemplateService(IManagementContext context) : base(context)
-        {
-        }
+	public class ReportConfigurationTemplateService : BaseCrudService<ReportConfigurationTemplate>
+	{
+		public ReportConfigurationTemplateService(IManagementContext context) : base(context)
+		{
+		}
 
-        public override ReportConfigurationTemplate Get(Guid id)
-        {
-            var result = Context.ReportConfigurationTemplate
-                .First(u => u.Id == id);
+		public override ReportConfigurationTemplate Get(Guid id)
+		{
+			var result = Context.ReportConfigurationTemplate
+				.First(u => u.Id == id);
 
-            return result;
-        }
+			return result;
+		}
 
-        public List<ReportConfigurationTemplate> GetPlaceholders(List<Guid> allowedDepartmentIds = null)
-        {
-            var query =
-                from template in Context.ReportConfigurationTemplate
-                where template.IsActive && ContainsAllowedDepartmentId(allowedDepartmentIds, template.IdFireSafetyDepartment)
-                select new ReportConfigurationTemplate
-                {
-                    Id = template.Id,
-                    Name = template.Name,
-                    IdFireSafetyDepartment = template.IdFireSafetyDepartment,
-                    IsDefault = template.IsDefault
-                };
+		public List<ReportConfigurationTemplate> GetPlaceholders(List<Guid> allowedDepartmentIds = null)
+		{
+			var query =
+				from template in Context.ReportConfigurationTemplate
+				where template.IsActive && ContainsAllowedDepartmentId(allowedDepartmentIds, template.IdFireSafetyDepartment)
+				select new ReportConfigurationTemplate
+				{
+					Id = template.Id,
+					Name = template.Name,
+					IdFireSafetyDepartment = template.IdFireSafetyDepartment,
+					IsDefault = template.IsDefault
+				};
 
-            return query.ToList();
-        }
-        
-        public ReportConfigurationTemplate CopyReportConfiguration(Guid idReport)
+			return query.ToList();
+		}
+		
+		public ReportConfigurationTemplate CopyReportConfiguration(Guid idReport)
 		{
 			var reportTemplate = Context.ReportConfigurationTemplate.AsNoTracking()
-                .First(u => u.Id == idReport);
+				.First(u => u.Id == idReport);
 
 			if (reportTemplate != null)
 			{
-                ReportConfigurationTemplate newReportTemplate = new ReportTemplateDuplicator().DuplicateReportTemplate(reportTemplate);
+				ReportConfigurationTemplate newReportTemplate = new ReportTemplateDuplicator().DuplicateReportTemplate(reportTemplate);
 
 				Context.ReportConfigurationTemplate.Add(newReportTemplate);
 				Context.SaveChanges();
@@ -54,12 +54,12 @@ namespace Survi.Prevention.ServiceLayer.Services
 			return reportTemplate;
 		}	
 
-        public override Guid AddOrUpdate(ReportConfigurationTemplate entity)
+		public override Guid AddOrUpdate(ReportConfigurationTemplate entity)
 		{
-            if (entity.IsDefault)
-            {
-                RemovePreviousDefault(entity.IdFireSafetyDepartment);
-            }
+			if (entity.IsDefault)
+			{
+				RemovePreviousDefault(entity.IdFireSafetyDepartment);
+			}
 
 			var isExistRecord = Context.Set<ReportConfigurationTemplate>().Any(c => c.Id == entity.Id);
 
@@ -72,28 +72,28 @@ namespace Survi.Prevention.ServiceLayer.Services
 			return entity.Id;
 		}
 
-        private bool ContainsAllowedDepartmentId(List<Guid> allowedDepartmentIds, Guid templateFireDepartmentId)
-        {
-            if (allowedDepartmentIds == null)
-                return false;
-            return (allowedDepartmentIds.Contains(templateFireDepartmentId)) || templateFireDepartmentId == Guid.Empty;
-        }
+		private bool ContainsAllowedDepartmentId(List<Guid> allowedDepartmentIds, Guid templateFireDepartmentId)
+		{
+			if (allowedDepartmentIds == null)
+				return false;
+			return (allowedDepartmentIds.Contains(templateFireDepartmentId)) || templateFireDepartmentId == Guid.Empty;
+		}
 
-        private void RemovePreviousDefault(Guid? idFireSafetyDepartment)
-        {
-                var query =
-                from template in Context.ReportConfigurationTemplate
-                where template.IdFireSafetyDepartment == idFireSafetyDepartment && template.IsDefault
-                select template;
+		private void RemovePreviousDefault(Guid? idFireSafetyDepartment)
+		{
+				var query =
+				from template in Context.ReportConfigurationTemplate
+				where template.IdFireSafetyDepartment == idFireSafetyDepartment && template.IsDefault
+				select template;
 
-                query.ToList();
+				query.ToList();
 
-                foreach (ReportConfigurationTemplate template in query) {
-                    template.IsDefault = false;
-                    Context.Set<ReportConfigurationTemplate>().Update(template);
-                }
+				foreach (ReportConfigurationTemplate template in query) {
+					template.IsDefault = false;
+					Context.Set<ReportConfigurationTemplate>().Update(template);
+				}
 
-                Context.SaveChanges();
-        }
-    }
+				Context.SaveChanges();
+		}
+	}
 }
