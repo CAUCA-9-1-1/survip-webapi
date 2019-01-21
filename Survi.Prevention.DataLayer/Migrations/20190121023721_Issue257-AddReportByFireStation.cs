36a7b16b35c43dba8cc6a6f82eec7b5e8d704c88
audrey <audrey.lebelr@gmail.com>
2019-01-20 21:40:32 -0500

    feat(#257): Added migration

---
+++ Survi.Prevention.DataLayer/Migrations/20190121023721_Issue257-AddReportByFireStation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Survi.Prevention.DataLayer.Migrations
+{
+    public partial class Issue257AddReportByFireStation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "id_fire_safety_department",
+                table: "report_configuration_template",
+                nullable: false,
+                defaultValue: Guid.Empty);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_default",
+                table: "report_configuration_template",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "id_fire_safety_department",
+                table: "report_configuration_template");
+
+            migrationBuilder.DropColumn(
+                name: "is_default",
+                table: "report_configuration_template");
+        }
+    }
+}
