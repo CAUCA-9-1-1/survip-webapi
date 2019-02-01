b549c00937bbc17502f3aeef757290b73a37b74f
Frederic Catellier <syd_freak@hotmail.com>
2019-02-01 11:54:25 -0500

    Fix(#301) Make the innerException parameter optional.

--- Survi.Prevention.ApiClient/Exceptions/BadParameterApiException.cs
+++ Survi.Prevention.ApiClient/Exceptions/BadParameterApiException.cs
@@ -4,7 +4,7 @@ namespace Survi.Prevention.ApiClient.Exceptions
 {
 	public class BadParameterApiException : ApiClientException
 	{
-		public BadParameterApiException(string url, Exception innerException) : base($"API returned a 400 (bad request) response for url '{url}'.", innerException)
+		public BadParameterApiException(string url, Exception innerException = null) : base($"API returned a 400 (bad request) response for url '{url}'.", innerException)
 		{
 		}
 	}
