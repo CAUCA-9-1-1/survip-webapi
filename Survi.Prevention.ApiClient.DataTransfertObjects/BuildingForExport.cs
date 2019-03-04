7080def1215c4ddb92e9bc9345c27d283b37b92c
Philippe Robert <philippe.robert@cauca.ca>
2019-03-04 14:04:07 -0500

    feat(#292): Add missing fields for prevention to cad export

--- Survi.Prevention.ApiClient.DataTransfertObjects/BuildingForExport.cs
+++ Survi.Prevention.ApiClient.DataTransfertObjects/BuildingForExport.cs
@@ -6,6 +6,7 @@ namespace Survi.Prevention.ApiClient.DataTransferObjects
    public class BuidldingForExport: BaseTransferObject
     {
         public Guid IdBuilding { get; set; }
+        public Guid IdCity { get; set; }
         public string CityName { get; set; }
         public string Address { get; set; }
         public string Name { get; set; }
