bedde93919fb1bd67b39ad9eb681c71c36811139
ALR <audrey.lebelr@gmail.com>
2019-03-09 13:25:51 -0500

    Feat(#404) Returning informations for statistics

---
+++ Survi.Prevention.WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.Models.DataTransfertObjects;
+using Survi.Prevention.Models.FireSafetyDepartments;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+    [Route("api/Statistics")]
+    public class StatisticsController : Controller
+    {
+        private readonly StatisticsService service;
+
+        public StatisticsController(StatisticsService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public ActionResult GetStatistics()
+        {
+            return Ok(service.GetStatistics());
+        }
+    }
+}
