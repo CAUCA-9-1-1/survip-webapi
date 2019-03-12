96deef1c38439013fed762c741ef05025437ac7d
Frederic Catellier <syd_freak@hotmail.com>
2019-03-12 14:18:51 -0400

    Feat(#318) Remove building localization and add alias + corporateName to building.

--- Survi.Prevention.ServiceLayer/Import/BaseImportWithPictureValidator.cs
+++ Survi.Prevention.ServiceLayer/Import/BaseImportWithPictureValidator.cs
@@ -20,4 +20,14 @@ namespace Survi.Prevention.ServiceLayer.Import
                 .Must(new BaseLocalizationValidator().HaveLocalizationNames).WithMessage("{PropertyName}_InvalidValue");
         }
     }
+
+    public abstract class BaseImportValidator<T> : AbstractValidator<T>
+        where T : BaseTransferObject, new()
+    {
+        protected BaseImportValidator()
+        {
+            RuleFor(m => m.Id)
+                .NotNullOrEmpty();
+        }
+    }
 }
