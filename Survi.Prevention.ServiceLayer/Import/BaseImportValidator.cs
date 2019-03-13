5b0aa37b588d5820be23239da320eafb33bd40b4
Frederic Catellier <syd_freak@hotmail.com>
2019-03-13 08:50:24 -0400

    Refactor(#493) only one class per file.

--- Survi.Prevention.ServiceLayer/Import/BaseImportValidator.cs
+++ Survi.Prevention.ServiceLayer/Import/BaseImportValidator.cs
@@ -4,18 +4,13 @@ using Survi.Prevention.ServiceLayer.ValidationUtilities;
 
 namespace Survi.Prevention.ServiceLayer.Import
 {
-    /*public abstract class BaseImportValidator<T>: AbstractValidator<T> where T : BaseLocalizableTransferObject,new()
+    public abstract class BaseImportValidator<T> : AbstractValidator<T>
+        where T : BaseTransferObject, new()
     {
-	    protected BaseImportValidator()
-	    {
-	        RuleFor(m => m.Id)
-	            .NotNullOrEmpty();
-
-		    RuleFor(m => m.Localizations)
-			    .NotNull().WithMessage("{PropertyName}_NullValue")
-			    .Must(new BaseLocalizationValidator().HaveRequiredLocalizationCount).WithMessage("{PropertyName}_InvalidCount")
-			    .Must(new BaseLocalizationValidator().HaveRequiredLanguages).WithMessage("{PropertyName}_InvalidValue")
-			    .Must(new BaseLocalizationValidator().HaveLocalizationNames).WithMessage("{PropertyName}_InvalidValue");
-	    }
-    }*/
+        protected BaseImportValidator()
+        {
+            RuleFor(m => m.Id)
+                .NotNullOrEmpty();
+        }
+    }
 }
