7428c509b5d9d056c312ebb464a332c4d9f320d5
Frederic Catellier <syd_freak@hotmail.com>
2019-03-20 10:47:29 -0400

    Feat(#504) Make all of HazrdousMaterial's fields available.

--- Survi.Prevention.ServiceLayer/Reporting/ReportBuildingHazardousMaterialGroupHandler.cs
+++ Survi.Prevention.ServiceLayer/Reporting/ReportBuildingHazardousMaterialGroupHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using Survi.Prevention.ApiClient.DataTransferObjects;
 using Survi.Prevention.Models.DataTransfertObjects;
+using Survi.Prevention.ServiceLayer.Localization.Base;
 using Survi.Prevention.ServiceLayer.Services;
 
 namespace Survi.Prevention.ServiceLayer.Reporting
 {
-	public class ReportBuildingHazardousMaterialGroupHandler : BaseReportGroupHandler<BuildingHazardousMaterialForList>
+	public class ReportBuildingHazardousMaterialGroupHandler : BaseReportGroupHandler<BuildingHazardousMaterialForReport>
 	{
 		private readonly BuildingHazardousMaterialService service;
 		protected override ReportBuildingGroup Group => ReportBuildingGroup.BuildingHazardousMaterial;
@@ -15,9 +17,9 @@ namespace Survi.Prevention.ServiceLayer.Reporting
 			this.service = service;
 		}
 
-		protected override List<BuildingHazardousMaterialForList> GetData(Guid idParent, string languageCode)
+		protected override List<BuildingHazardousMaterialForReport> GetData(Guid idParent, string languageCode)
 		{
-			return service.GetMaterialsForList(idParent, languageCode);
+			return service.GetMaterialsForReport(idParent, languageCode);
 		}
 
 		public static (string Group, List<string> Placeholders) GetPlaceholders()
@@ -25,5 +27,17 @@ namespace Survi.Prevention.ServiceLayer.Reporting
 			var placeholders = GetPlaceholderList();
 			return (ReportBuildingGroup.BuildingHazardousMaterial.ToString(), placeholders);
 		}
-	}
+
+	    protected override string FormatPropertyValue((string name, object value) property, string languageCode)
+	    {
+	        if (property.value is StorageTankType type)
+	            return GetTankTypeLocalized(type, languageCode);
+	        return base.FormatPropertyValue(property, languageCode);
+	    }
+
+	    private string GetTankTypeLocalized(StorageTankType tankType, string languageCode)
+	    {
+	        return tankType.GetDisplayName(languageCode);
+	    }
+    }
 }
