be8c5045cb62b6b23d6e78170bfa2ad928d677f6
Philippe Robert <philippe.robert@cauca.ca>
2019-04-02 14:49:14 -0400

    feat(#319):building grouping on inspection export

--- Survi.Prevention.ServiceLayer/Services/InspectionBuildingService.cs
+++ Survi.Prevention.ServiceLayer/Services/InspectionBuildingService.cs
@@ -10,109 +10,112 @@ using Survi.Prevention.Models.InspectionManagement;
 
 namespace Survi.Prevention.ServiceLayer.Services
 {
-	public class InspectionBuildingService : BaseService
-	{
-		public InspectionBuildingService(IManagementContext context) : base(context)
-		{
-		}
-
-		public List<BatchInspectionBuilding> GetBuildingForManagement(Guid batchId, string languageCode)
-		{
-			var query =
-				from building in Context.BatchInspectionBuildings.AsNoTracking()
-				where building.LanguageCode == languageCode && building.IdBatch == batchId
-				select building;
-
-			return query.ToList();
-		}
-
-		public List<InspectionBuildingForList> GetBuildings(Guid inspectionId, string languageCode)
-		{
-			var results = (
-				from inspection in Context.Inspections.AsNoTracking()
-				where inspection.Id == inspectionId && inspection.IsActive
-				from building in inspection.Buildings
-				where building.IsActive
-				select new
-				{
-					building.Id,
-					building.AliasName,
-                    building.CorporateName,
-                    building.ChildType,
-					building.Picture.Data
-				}).ToList();
-
-			return results
-				.Select(building => new InspectionBuildingForList
-				{
-					Id = building.Id,
-					IdInspection = inspectionId,
-                    IsParent = building.ChildType == 0,
-                    AliasName = building.AliasName,
-                    CorporateName = building.CorporateName,
-					Picture = building.Data == null ? null : Convert.ToBase64String(building.Data)
-				})
-                .ToList();
-		}
-
-	    public List<InspectionBuildingResume> GetBuildingsResume(Guid inspectionId)
-	    {
-	        var query =
-	            from building in Context.InspectionBuildings.AsNoTracking()
-	            where building.IsActive && building.IdInspection == inspectionId
-	            select new InspectionBuildingResume
-	            {
-	                IdBuilding = building.Id,
-	                AliasName = building.AliasName,
-	                CorporateName = building.CorporateName,
-	                IsMainBuilding = building.ChildType == BuildingChildType.None,
-	                Coordinates = building.Coordinates,
-	                IdLaneTransversal = building.IdLaneTransversal
-	            };
-
-	        var result = query.ToList();
-	        return result;
-	    }
-
-        public List<InspectionForExport> GetInspectionForExport()
+    public class InspectionBuildingService : BaseService
+    {
+        public InspectionBuildingService(IManagementContext context) : base(context)
         {
-            var query = (from inspection in Context.Inspections.AsNoTracking()
-                where inspection.IsActive && inspection.Status == InspectionStatus.Approved
-                from building in Context.Buildings
-                where building.IsActive && building.Id == inspection.IdBuilding &&
-                      !string.IsNullOrEmpty(building.IdExtern) &&
-                      building.HasBeenModified
-                from cityLoc in building.City.Localizations.Where(cl => cl.LanguageCode == "fr" && cl.IsActive)
-                from laneLocalization in building.Lane.Localizations.Where(ll => ll.LanguageCode == "fr" && ll.IsActive)
-                         select new InspectionForExport
-                {
-                    CityName = cityLoc.Name,
-                    Name = building.AliasName,
-                    Id = inspection.Id,
-                    IdBuilding = inspection.IdBuilding,
-                    IdCity = building.City.IdExtern,
-                    Address = new AddressGenerator().GenerateAddress(building.CivicNumber, building.CivicLetter, laneLocalization.Name, building.Lane.LaneGenericCode.Description, building.Lane.PublicCode.Description, building.Lane.LaneGenericCode.AddWhiteSpaceAfter),
-                    LastEditedOn = inspection.LastModifiedOn ?? DateTime.Now,
-                    IdBuildingExtern = building.IdExtern
-                });
+        }
+
+        public List<BatchInspectionBuilding> GetBuildingForManagement(Guid batchId, string languageCode)
+        {
+            var query =
+                from building in Context.BatchInspectionBuildings.AsNoTracking()
+                where building.LanguageCode == languageCode && building.IdBatch == batchId
+                select building;
 
             return query.ToList();
         }
 
-	    public bool SaveBuildingsResume(InspectionBuildingResume building)
-	    {
-	        var currentBuilding = Context.InspectionBuildings.FirstOrDefault(b => b.Id == building.IdBuilding);
-	        if (currentBuilding != null)
-	        {
-	            currentBuilding.IdLaneTransversal = building.IdLaneTransversal;
-	            currentBuilding.AliasName = building.AliasName;
-	            currentBuilding.CorporateName = building.CorporateName;
-	        }
-	        else
-	            return false;
+        public List<InspectionBuildingForList> GetBuildings(Guid inspectionId, string languageCode)
+        {
+            var results = (
+                from inspection in Context.Inspections.AsNoTracking()
+                where inspection.Id == inspectionId && inspection.IsActive
+                from building in inspection.Buildings
+                where building.IsActive
+                select new
+                {
+                    building.Id,
+                    building.AliasName,
+                    building.CorporateName,
+                    building.ChildType,
+                    building.Picture.Data
+                }).ToList();
 
-	        Context.SaveChanges();
-	        return true;
-	    }
-	}
+            return results
+                .Select(building => new InspectionBuildingForList
+                {
+                    Id = building.Id,
+                    IdInspection = inspectionId,
+                    IsParent = building.ChildType == 0,
+                    AliasName = building.AliasName,
+                    CorporateName = building.CorporateName,
+                    Picture = building.Data == null ? null : Convert.ToBase64String(building.Data)
+                })
+                .ToList();
+        }
+
+        public List<InspectionBuildingResume> GetBuildingsResume(Guid inspectionId)
+        {
+            var query =
+                from building in Context.InspectionBuildings.AsNoTracking()
+                where building.IsActive && building.IdInspection == inspectionId
+                select new InspectionBuildingResume
+                {
+                    IdBuilding = building.Id,
+                    AliasName = building.AliasName,
+                    CorporateName = building.CorporateName,
+                    IsMainBuilding = building.ChildType == BuildingChildType.None,
+                    Coordinates = building.Coordinates,
+                    IdLaneTransversal = building.IdLaneTransversal
+                };
+
+            var result = query.ToList();
+            return result;
+        }
+
+        public List<InspectionForExport> GetInspectionForExport()
+        {
+            var query =
+                from building in Context.Buildings.AsNoTracking()
+                where building.IsActive &&
+                      !string.IsNullOrEmpty(building.IdExtern) &&
+                      building.HasBeenModified
+                let inspection = Context.Inspections
+                    .Where(i => i.IsActive && i.Status == InspectionStatus.Approved && i.IdBuilding == building.Id)
+                    .OrderByDescending(i => i.CompletedOn)
+                    .LastOrDefault()
+                from cityLoc in building.City.Localizations.Where(cl => cl.LanguageCode == "fr" && cl.IsActive)
+                from laneLocalization in building.Lane.Localizations.Where(ll => ll.LanguageCode == "fr" && ll.IsActive)
+                select new InspectionForExport
+                {
+                    CityName = cityLoc.Name,
+                    Name = building.AliasName,
+                    Id = inspection.Id,
+                    IdBuilding = building.Id,
+                    IdCity = building.City.IdExtern,
+                    Address = new AddressGenerator().GenerateAddress(building.CivicNumber, building.CivicLetter, laneLocalization.Name, building.Lane.LaneGenericCode.Description, building.Lane.PublicCode.Description, building.Lane.LaneGenericCode.AddWhiteSpaceAfter),
+                    LastEditedOn = inspection.LastModifiedOn ?? DateTime.Now,
+                    IdBuildingExtern = building.IdExtern
+                };
+
+            return query.ToList();
+        }
+
+        public bool SaveBuildingsResume(InspectionBuildingResume building)
+        {
+            var currentBuilding = Context.InspectionBuildings.FirstOrDefault(b => b.Id == building.IdBuilding);
+            if (currentBuilding != null)
+            {
+                currentBuilding.IdLaneTransversal = building.IdLaneTransversal;
+                currentBuilding.AliasName = building.AliasName;
+                currentBuilding.CorporateName = building.CorporateName;
+            }
+            else
+                return false;
+
+            Context.SaveChanges();
+            return true;
+        }
+    }
 }
