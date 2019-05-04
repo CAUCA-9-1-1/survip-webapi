318f2ad8240070dfc8b849c7d57f4b581b35224a
Audrey Lebel <audrey.lebelr@gmail.com>
2019-05-03 20:33:02 -0400

    Feat(#404) Refactored code

---
+++ Survi.Prevention.Models/DataTransfertObjects/InspectionVisitForStatistics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Survi.Prevention.Models.InspectionManagement;
+
+namespace Survi.Prevention.Models.DataTransfertObjects
+{
+    public class InspectionVisitForStatistics
+    {
+		public Guid Id { get; set; }
+        public InspectionVisitStatus Status { get; set; }
+		public int RiskLevel { get; set; }
+        public Guid IdCity { get; set; }
+		public DateTime? CompletedOn { get; set; }
+    }
+}
