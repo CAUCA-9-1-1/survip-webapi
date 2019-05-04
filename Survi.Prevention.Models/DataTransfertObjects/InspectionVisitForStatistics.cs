64140568591ebb09fc1abb55fcf39971f918c7d0
Audrey Lebel <audrey.lebelr@gmail.com>
2019-05-03 23:17:25 -0400

    Feat(#404) FIxed sending city instead of SSI

--- Survi.Prevention.Models/DataTransfertObjects/InspectionVisitForStatistics.cs
+++ Survi.Prevention.Models/DataTransfertObjects/InspectionVisitForStatistics.cs
@@ -5,10 +5,13 @@ namespace Survi.Prevention.Models.DataTransfertObjects
 {
     public class InspectionVisitForStatistics
     {
-		public Guid Id { get; set; }
+        public Guid Id { get; set; }
         public InspectionVisitStatus Status { get; set; }
-		public int RiskLevel { get; set; }
-        public Guid IdCity { get; set; }
-		public DateTime? CompletedOn { get; set; }
+        public int RiskLevel { get; set; }
+        public Guid IdFireSafetyDepartment { get; set; }
+        public DateTime? CompletedOn { get; set; }
+        public Boolean HasBeenRefused { get; set; }
+        public Boolean OwnerWasAbsent { get; set; }
+        public Boolean DoorHangerHasBeenLeft { get; set; }
     }
 }
