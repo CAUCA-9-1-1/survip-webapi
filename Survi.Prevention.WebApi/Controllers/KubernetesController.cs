04c0ead0c59840f2c3cf29686129c4626eaeafd7
Frederic Catellier <syd_freak@hotmail.com>
2019-05-06 14:33:25 -0400

    Feat: Add an health check for the web api.

---
+++ Survi.Prevention.WebApi/Controllers/KubernetesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Survi.Prevention.ServiceLayer.Services;
+
+namespace Survi.Prevention.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KubernetesController : ControllerBase
+    {
+        private readonly WebuserService service;
+
+        public KubernetesController(WebuserService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet, Route("/Healthz"), AllowAnonymous]
+        public ActionResult GetHealthz()
+        {
+            try
+            {
+
+                if (service.GetList().Any())
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
