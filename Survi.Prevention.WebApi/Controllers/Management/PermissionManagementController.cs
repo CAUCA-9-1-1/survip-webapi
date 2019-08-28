4b9b19657d7906b38a8e93335620cab4d9612544
Yannick <yannick.jacques@cauca.ca>
2019-08-28 14:47:46 -0400

    PRE-430 Ajouter les controlleurs pour la gestion des groupes et permissions

---
+++ Survi.Prevention.WebApi/Controllers/Management/PermissionManagementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cause.SecurityManagement.Controllers;
+using Cause.SecurityManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Survi.Prevention.WebApi.Controllers.Management
+{
+	[Route("api/[Controller]")]
+	public class PermissionManagementController : BasePermissionManagementController
+	{
+		public PermissionManagementController(IPermissionManagementService permissionService) : base(permissionService)
+		{
+		}
+	}
+}
